432e55aae4a1db2771cf347956fcf6a6efd43a33
Michael Kelly <mkelly@mozilla.com>
2015-12-22 14:00:41 -0800

    omg omg omg

---
+++ normandy/settings.py
@@ -0,0 +1,98 @@
+import os
+
+from configurations import Configuration, values
+
+
+class Core(Configuration):
+    """Settings that will never change per-environment."""
+    # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+    # Application definition
+    INSTALLED_APPS = [
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+    ]
+
+    MIDDLEWARE_CLASSES = [
+        'django.middleware.security.SecurityMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ]
+
+    ROOT_URLCONF = 'normandy.urls'
+
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+            },
+        },
+    ]
+
+    WSGI_APPLICATION = 'normandy.wsgi.application'
+
+    # Internationalization
+    LANGUAGE_CODE = 'en-us'
+    TIME_ZONE = 'UTC'
+    USE_I18N = False
+    USE_L10N = False
+    USE_TZ = True
+
+    # Static files (CSS, JavaScript, Images)
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+class Base(Core):
+    """Settings that may change per-environment, some with defaults."""
+    SECRET_KEY = values.SecretValue()
+    DEBUG = values.BooleanValue(False)
+    ALLOWED_HOSTS = values.ListValue([])
+    DATABASES = values.DatabaseURLValue('sqlite:///db.sqlite3')
+
+    # Password validation
+    AUTH_PASSWORD_VALIDATORS = [
+        {
+            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        },
+    ]
+
+
+class Development(Base):
+    """Settings for local development."""
+    DOTENV = '.env'
+    SECRET_KEY = values.Value('not a secret')
+    DEBUG = values.BooleanValue(True)
+    AUTH_PASSWORD_VALIDATORS = values.ListValue([])
+
+
+class Production(Base):
+    """Settings for the production environment."""
+    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
