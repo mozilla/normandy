df196045cda7cfdb5a76f63cbfe082fa11108f40
Mike Cooper <mythmon@gmail.com>
2016-01-19 16:06:16 -0800

    Remove counters

--- normandy/recipes/models.py
+++ normandy/recipes/models.py
@@ -6,7 +6,6 @@ from django.db import models
 from adminsortable.models import SortableMixin
 from django_countries.fields import CountryField
 
-from normandy.counters import get_counter
 from normandy.recipes.fields import AutoHashField, LocaleField, PercentField
 from normandy.recipes import utils
 
@@ -25,7 +24,6 @@ class Recipe(models.Model):
     start_time = models.DateTimeField(blank=True, null=True, default=None)
     end_time = models.DateTimeField(blank=True, null=True, default=None)
     sample_rate = PercentField(default=100)
-    count_limit = models.PositiveIntegerField(blank=True, null=True, default=None)
 
     @property
     def implementation(self):
@@ -66,10 +64,6 @@ class Recipe(models.Model):
                 self.log_rejection('did not match sample')
                 return False
 
-        if self.count_limit is not None and not get_counter().check(self):
-            self.log_rejection('over counter')
-            return False
-
         return True
 
     def __repr__(self):
@@ -77,10 +71,6 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.name
-
-    @property
-    def has_counter(self):
-        return self.count_limit is not None
 
 
 class Action(models.Model):
