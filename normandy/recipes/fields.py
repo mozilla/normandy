6f729b58fb27a4caf7d6f139a158b2fd080e815a
Michael Kelly <mkelly@mozilla.com>
2016-01-22 10:28:10 -0800

    Remove choices on LocaleField.
    Having LocaleField load it's choices caused product_details to
    attempt to load data from the database before the migrations were
    run during tests. Since we're not using the field anymore, empty
    choices is fine.

--- normandy/recipes/fields.py
+++ normandy/recipes/fields.py
@@ -3,19 +3,12 @@ import hashlib
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-from product_details import product_details
-
 
 class LocaleField(models.CharField):
     """Legacy field leftover to make earlier migrations work."""
-    CHOICES = {
-        code: names['English']
-        for code, names in product_details.languages.items()
-    }
-
     def __init__(self, *args, **kwargs):
         kwargs.setdefault('max_length', 255)
-        kwargs.setdefault('choices', self.CHOICES.items())
+        kwargs.setdefault('choices', {})
         return super().__init__(*args, **kwargs)
 
 
