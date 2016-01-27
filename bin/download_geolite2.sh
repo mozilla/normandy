8cc8d3fac2c431e9b91b90e8c9783eff919bb9de
Michael Kelly <mkelly@mozilla.com>
2016-01-27 10:10:49 -0800

    Generate SHA-256 cert for SSL dev server.
    The certificate included with runsslserver is SHA1 and will stop
    working with Firefox soon, and having all the devs trust a single cert
    instead of generating their own personal ones is a bad idea anyway.

--- bin/download_geolite2.sh
+++ bin/download_geolite2.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 # This script downloads and extracts the free GeoLite2 country database
 # from MaxMind for use in development.
-BASE_DIR="$(dirname "$(dirname "$0")")"
+BASE_DIR="$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )")"
 DOWNLOAD_URL=http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz
 LOCAL_ARCHIVE=/tmp/GeoLite2-Country.mmdb.gz
 LOCAL_EXTRACTED=/tmp/GeoLite2-Country.mmdb
