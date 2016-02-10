faafe7936d90615808f127871f37a5a4946da151
Daniel Thorn <dthorn@mozilla.com>
2016-02-10 11:17:00 -0800

    use travis deploy and assume secrets are present

--- bin/deploy/dockerhub.sh
+++ bin/deploy/dockerhub.sh
@@ -4,27 +4,24 @@
 
 set -e
 
-# do nothing unless we have docker creds
-if [ -n "$DOCKER_EMAIL" -a -n "$DOCKER_USERNAME" -a -n "$DOCKER_PASSWORD" ]; then
-    # configure docker creds
-    docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+# configure docker creds
+docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 
-    # docker tag and push git branch to dockerhub
-    if [ -n "$TRAVIS_BRANCH" ]; then
-        [ "$TRAVIS_BRANCH" == master ] && TAG=latest || TAG="$TRAVIS_BRANCH"
-        docker tag normandy:build "mozilla/normandy:$TAG" ||
-            (echo "Couldn't tag normandy:build as mozilla/normandy:$TAG" && false)
-        docker push "mozilla/normandy:$TAG" ||
-            (echo "Couldn't push mozilla/normandy:$TAG" && false)
-        echo "Pushed mozilla/normandy:$TAG"
-    fi
+# docker tag and push git branch to dockerhub
+if [ -n "$TRAVIS_BRANCH" ]; then
+    [ "$TRAVIS_BRANCH" == master ] && TAG=latest || TAG="$TRAVIS_BRANCH"
+    docker tag normandy:build "mozilla/normandy:$TAG" ||
+        (echo "Couldn't tag normandy:build as mozilla/normandy:$TAG" && false)
+    docker push "mozilla/normandy:$TAG" ||
+        (echo "Couldn't push mozilla/normandy:$TAG" && false)
+    echo "Pushed mozilla/normandy:$TAG"
+fi
 
-    # docker tag and push git tag to dockerhub
-    if [ -n "$TRAVIS_TAG" ]; then
-        docker tag normandy:build "mozilla/normandy:$TRAVIS_TAG" ||
-            (echo "Couldn't tag normandy:build as mozilla/normandy:$TRAVIS_TAG" && false)
-        docker push "mozilla/normandy:$TRAVIS_TAG" ||
-            (echo "Couldn't push mozilla/normandy:$TRAVIS_TAG" && false)
-        echo "Pushed mozilla/normandy:$TRAVIS_TAG"
-    fi
+# docker tag and push git tag to dockerhub
+if [ -n "$TRAVIS_TAG" ]; then
+    docker tag normandy:build "mozilla/normandy:$TRAVIS_TAG" ||
+        (echo "Couldn't tag normandy:build as mozilla/normandy:$TRAVIS_TAG" && false)
+    docker push "mozilla/normandy:$TRAVIS_TAG" ||
+        (echo "Couldn't push mozilla/normandy:$TRAVIS_TAG" && false)
+    echo "Pushed mozilla/normandy:$TRAVIS_TAG"
 fi
