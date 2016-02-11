1e6951e0d8a4dda36540339ae359950aa693cd55
Mike Cooper <mythmon@gmail.com>
2016-02-11 15:53:00 -0800

    Provide the expected console to actions.

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const {RecipeRunner} = require('./lib/RecipeRunner.js');
 const {SelfRepairInteraction} = require('./lib/SelfRepairInteraction.js');
-const {Log} = require('./lib/Log.js');
 
 exports.main = function({loadReason}) {
   SelfRepairInteraction.disableSelfRepair();
-  Log.trace(`main: loadReason=${loadReason}`);
-  RecipeRunner.init({waitForTabs: loadReason === 'startup'});
+  RecipeRunner.init();
 };
 
 exports.onUnload = function() {
   SelfRepairInteraction.enableSelfRepair();
-  RecipeRunner.uninit();
 };
