b94385ec790049681fcafbb03ae9edd01ba74c99
Brittany Storoz <brittanystoroz@gmail.com>
2016-04-07 13:16:42 -0400

    Subclass LoginRequiredMixin

--- normandy/control/views.py
+++ normandy/control/views.py
@@ -4,16 +4,18 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from normandy.recipes.models import Recipe
 
 
-class IndexView(LoginRequiredMixin, generic.ListView):
+class ControlMixin(LoginRequiredMixin):
+  login_url = '/control/login/'
+
+
+class IndexView(ControlMixin, generic.ListView):
   template_name = 'control/index.html'
   context_object_name = 'all_recipes_list'
-  login_url = '/control/login/'
 
   def get_queryset(self):
     return Recipe.objects.all()
 
 
-class EditView(LoginRequiredMixin, generic.DetailView):
+class EditView(ControlMixin, generic.DetailView):
   model = Recipe
   template_name = 'control/edit_recipe.html'
-  login_url = '/control/login/'
