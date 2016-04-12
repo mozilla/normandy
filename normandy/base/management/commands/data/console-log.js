b5a02924c248d1c66c4af545e14e5805ccf187b7
Michael Kelly <mkelly@mozilla.com>
2016-04-12 10:04:41 -0700

    Add a default action as part of the initial_data command.

---
+++ normandy/base/management/commands/data/console-log.js
@@ -0,0 +1,15 @@
+function ConsoleLogAction(normandy, recipe) {
+    this.normandy = normandy;
+    this.recipe = recipe;
+}
+
+ConsoleLogAction.prototype.execute = function() {
+    var normandy = this.normandy;
+    var recipe = this.recipe;
+
+    return new Promise(function(resolve) {
+        normandy.log(recipe.arguments.message, 'info');
+    });
+};
+
+window.registerAction('console-log', ConsoleLogAction);
