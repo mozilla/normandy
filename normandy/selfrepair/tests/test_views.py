f72b703896c621e664f33a903ce5f382dab71241
Mike Cooper <mythmon@gmail.com>
2016-04-28 12:38:09 -0700

    Making trailing slash on repair url optional
    Fixes bug 1268222.

--- normandy/selfrepair/tests/test_views.py
+++ normandy/selfrepair/tests/test_views.py
@@ -5,11 +5,23 @@ from django.test.utils import CaptureQueriesContext
 import pytest
 
 
-@pytest.mark.django_db
-def test_selfrepair_makes_no_db_queries(client):
-    queries = CaptureQueriesContext(connection)
-    with queries:
-        url = reverse('selfrepair:index', args={'locale': 'en-US'})
-        res = client.get(url)
-        assert res.status_code == 200
-    assert len(queries) == 0
+class TestSelfRepair:
+    def test_url_is_right(self):
+        url = reverse('selfrepair:index', args=['en-US'])
+        assert url == '/en-US/repair'
+
+    @pytest.mark.django_db
+    def test_makes_no_db_queries(self, client):
+        queries = CaptureQueriesContext(connection)
+        with queries:
+            url = reverse('selfrepair:index', args=['en-US'])
+            res = client.get(url)
+            assert res.status_code == 200
+        assert len(queries) == 0
+
+    @pytest.mark.django_db
+    def test_doesnt_redirect(self, client):
+        url = '/en-US/repair'
+        assert client.get(url).status_code == 200
+        url += '/'
+        assert client.get(url).status_code == 200
