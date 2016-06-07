c2c359beb58de68b5e0206a6e84fa8670ea3d242
casebenton <casebenton@gmail.com>
2016-06-07 15:02:21 -0700

    Style updates
    removed unused import line, used single quotes for string

--- normandy/base/views.py
+++ normandy/base/views.py
@@ -1,7 +1,4 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("")
+    return HttpResponse('')
