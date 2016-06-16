b56f6aac1dd24d9b9ab7ca1296d68ba06334b542
Brittany Storoz <brittanystoroz@gmail.com>
2016-06-16 14:03:13 +0100

    Remove isVisible state from notification bar

--- normandy/control/static/control/js/components/NotificationBar.jsx
+++ normandy/control/static/control/js/components/NotificationBar.jsx
@@ -1,50 +1,34 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { setNotification } from '../actions/ControlActions.js'
+import ControlActions from '../actions/ControlActions.js'
 import classNames from 'classnames'
 
 class NotificationBar extends React.Component {
   constructor() {
     super();
-    this.state = {
-      isVisible: false
-    }
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.notification) {
-      this.setState({
-        isVisible: true
-      });
-      this.setVisibilityTimer();
+      setTimeout(() => {
+        this.removeNotification();
+      }, 10000);
     }
   }
 
-  setVisibilityTimer() {
-    this.visibilityTimer !== null ? clearTimeout(this.visibilityTimer) : null;
-
-    this.visibilityTimer = setTimeout(() => {
-      this.setState({
-        isVisible: false
-      })
-    }, 10000);
-  }
-
-  hideNotificationBar() {
-    this.setState({
-      isVisible: false
-    });
-    clearTimeout(this.visibilityTimer);
+  removeNotification() {
+    const { dispatch } = this.props;
+    dispatch(ControlActions.setNotification());
   }
 
   render() {
     const { notification, dispatch } = this.props;
     return (
-      <div id="notification-bar" className={classNames({ 'active': this.state.isVisible })}>
+      <div id="notification-bar" className={classNames({ 'active': notification })}>
         { notification &&
           <p className={classNames('notification', notification.messageType)}>
             { notification.message }
-            <i className="fa fa-lg fa-times remove-message" onClick={::this.hideNotificationBar}></i>
+            <i className="fa fa-lg fa-times remove-message" onClick={::this.removeNotification}></i>
           </p>
         }
       </div>
