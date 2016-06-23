e14301da7670e84d129ff362a9718858b57b8a9e
Mike Cooper <mythmon@gmail.com>
2016-06-23 15:42:39 -0700

    Use fetch-mock in Karma tests

--- normandy/control/tests/actions/controlActions.js
+++ normandy/control/tests/actions/controlActions.js
@@ -1,5 +1,7 @@
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
+import fetchMock from 'fetch-mock';
+
 import controlActions, * as actionTypes from '../../static/control/js/actions/ControlActions'
 import { fixtureRecipes, fixtureRevisions, initialState } from '../fixtures/fixtures';
 
@@ -26,6 +28,11 @@ describe('controlApp Actions', () => {
 
   beforeEach(() => {
     store.clearActions();
+    fetchMock.restore();
+  });
+
+  afterEach(() => {
+    expect(fetchMock.calls().unmatched).toEqual([]);
   });
 
   it('creates REQUEST_IN_PROGRESS when initiating an api call', () => {
@@ -40,7 +47,7 @@ describe('controlApp Actions', () => {
 
     it('returns with `status: success` if the request succeeded', () => {
       const expectedAction = { type: actionTypes.REQUEST_COMPLETE, status: 'success' };
-      spyOn(window, 'fetch').and.returnValue(successPromise(fixtureRecipes));
+      fetchMock.mock('/api/v1/recipe/?format=json&', 'GET', fixtureRecipes);
 
       return store.dispatch(controlActions.makeApiRequest('fetchAllRecipes')).then(() => {
         expect(store.getActions()).toContain(expectedAction);
@@ -49,7 +56,7 @@ describe('controlApp Actions', () => {
 
     it('returns with `status: error` if the request failed', () => {
       const expectedAction = { type: actionTypes.REQUEST_COMPLETE, status: 'error' };
-      spyOn(window, 'fetch').and.returnValue(failurePromise());
+      fetchMock.mock('/api/v1/recipe/?format=json&', 'GET', {status: 400});
 
       return store.dispatch(controlActions.makeApiRequest('fetchAllRecipes')).then(() => {
         expect(store.getActions()).toContain(expectedAction);
@@ -58,7 +65,7 @@ describe('controlApp Actions', () => {
 
     it('creates a SET_NOTIFICATION action if provided', () => {
       const expectedAction = { type: actionTypes.SET_NOTIFICATION, notification: { messageType: 'error', 'message': 'Error fetching recipes.'} };
-      spyOn(window, 'fetch').and.returnValue(failurePromise());
+      fetchMock.mock('/api/v1/recipe/?format=json&', 'GET', {status: 400});
 
       return store.dispatch(controlActions.makeApiRequest('fetchAllRecipes')).then(() => {
         expect(store.getActions()).toContain(expectedAction);
@@ -68,11 +75,10 @@ describe('controlApp Actions', () => {
 
   it('creates RECIPES_RECEIVED when fetching recipes is successful', () => {
     const expectedAction = { type: actionTypes.RECIPES_RECEIVED, recipes: fixtureRecipes }
-    spyOn(window, 'fetch').and.returnValue(successPromise(fixtureRecipes));
+      fetchMock.mock('/api/v1/recipe/?format=json&', 'GET', fixtureRecipes);
 
     return store.dispatch(controlActions.makeApiRequest('fetchAllRecipes')).then(() => {
-      expect(window.fetch).toHaveBeenCalled();
-      expect(window.fetch).toHaveBeenCalledWith('/api/v1/recipe/?format=json&', jasmine.any(Object));
+      expect(fetchMock.calls('/api/v1/recipe/?format=json&').length).toEqual(1);
       expect(store.getActions()).toContain(expectedAction);
     });
 
@@ -87,60 +93,55 @@ describe('controlApp Actions', () => {
 
   it('creates SINGLE_RECIPE_RECEIVED when fetching a single RECIPE is successful', () => {
     const expectedAction = { type: actionTypes.SINGLE_RECIPE_RECEIVED, recipe: fixtureRecipes[0] };
-    spyOn(window, 'fetch').and.returnValue(successPromise(fixtureRecipes[0]));
+    fetchMock.mock('/api/v1/recipe/1/?format=json&', 'GET', fixtureRecipes[0]);
 
     return store.dispatch(controlActions.makeApiRequest('fetchSingleRecipe', { recipeId: 1 }))
       .then(() => {
-        expect(window.fetch).toHaveBeenCalled();
-        expect(window.fetch).toHaveBeenCalledWith('/api/v1/recipe/1/?format=json&', jasmine.any(Object));
+        expect(fetchMock.calls('/api/v1/recipe/1/?format=json&').length).toEqual(1);
         expect(store.getActions()).toContain(expectedAction);
       })
   });
 
   it('creates SINGLE_RECIPE_RECEIVED when fetching a single REVISION is successful', () => {
     const expectedAction = { type: actionTypes.SINGLE_RECIPE_RECEIVED, recipe: fixtureRevisions[0].recipe };
-    spyOn(window, 'fetch').and.returnValue(successPromise(fixtureRevisions[0]));
+    fetchMock.mock('/api/v1/recipe_version/169/?format=json&', 'GET', fixtureRevisions[0]);
 
     return store.dispatch(controlActions.makeApiRequest('fetchSingleRevision', { revisionId: 169 }))
       .then(() => {
-        expect(window.fetch).toHaveBeenCalled();
-        expect(window.fetch).toHaveBeenCalledWith('/api/v1/recipe_version/169/?format=json&', jasmine.any(Object));
+        expect(fetchMock.calls('/api/v1/recipe_version/169/?format=json&').length).toEqual(1);
         expect(store.getActions()).toContain(expectedAction);
       })
   });
 
   it('creates RECIPE_ADDED when adding a recipe is successful', () => {
     const expectedAction = { type: actionTypes.RECIPE_ADDED, recipe: fixtureRecipes[0] }
-    spyOn(window, 'fetch').and.returnValue(successPromise(fixtureRecipes[0]));
+    fetchMock.mock('/api/v1/recipe/?format=json&', 'POST', fixtureRecipes[0]);
 
     return store.dispatch(controlActions.makeApiRequest('addRecipe', fixtureRecipes[0]))
       .then(() => {
-        expect(window.fetch).toHaveBeenCalled();
-        expect(window.fetch).toHaveBeenCalledWith('/api/v1/recipe/?format=json&', jasmine.any(Object));
+        expect(fetchMock.calls('/api/v1/recipe/?format=json&').length).toEqual(1);
         expect(store.getActions()).toContain(expectedAction);
       })
   });
 
   it('creates RECIPE_UPDATED when updating a recipe is successful', () => {
     const expectedAction = { type: actionTypes.RECIPE_UPDATED, recipe: fixtureRecipes[0] };
-    spyOn(window, 'fetch').and.returnValue(successPromise(fixtureRecipes[0]));
+    fetchMock.mock('/api/v1/recipe/1/?format=json&', 'PATCH', fixtureRecipes[0]);
 
     return store.dispatch(controlActions.makeApiRequest('updateRecipe', { recipe: fixtureRecipes[0], recipeId: 1 }))
       .then(() => {
-        expect(window.fetch).toHaveBeenCalled();
-        expect(window.fetch).toHaveBeenCalledWith('/api/v1/recipe/1/?format=json&', jasmine.any(Object));
+        expect(fetchMock.calls('/api/v1/recipe/1/?format=json&').length).toEqual(1);
         expect(store.getActions()).toContain(expectedAction);
       })
   });
 
   it('creates RECIPE_DELETED when deleting a recipe is successful', () => {
     const expectedAction = { type: actionTypes.RECIPE_DELETED, recipeId: 1 };
-    spyOn(window, 'fetch').and.returnValue(successPromise(fixtureRecipes));
+    fetchMock.mock('/api/v1/recipe/1/?format=json&', 'DELETE', fixtureRecipes[0]);
 
     return store.dispatch(controlActions.makeApiRequest('deleteRecipe', { recipeId: 1 }))
       .then(() => {
-        expect(window.fetch).toHaveBeenCalled();
-        expect(window.fetch).toHaveBeenCalledWith('/api/v1/recipe/1/?format=json&', jasmine.any(Object));
+        expect(fetchMock.calls('/api/v1/recipe/1/?format=json&').length).toEqual(1);
         expect(store.getActions()).toContain(expectedAction);
       })
   });
