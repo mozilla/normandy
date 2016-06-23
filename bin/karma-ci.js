a03b08dc22909058b3df69b6b51ec93b4209c28a
Mike Cooper <mythmon@gmail.com>
2016-06-23 11:00:08 -0700

    Add Karma tests to Circle CI

---
+++ bin/karma-ci.js
@@ -0,0 +1,19 @@
+var karma = require('karma');
+var karmaConfig = require('karma/lib/config');
+
+var config = karmaConfig.parseConfig(__dirname + '/../karma.conf.js', {
+  browsers: [],
+  oneShot: true,
+});
+
+var server = new karma.Server(config);
+
+server.on('browser_complete', (completedBrowser) => {
+  if (completedBrowser.lastResult.failed > 0) {
+    process.exit(1);
+  } else {
+    process.exit(0);
+  }
+});
+
+server.start();
