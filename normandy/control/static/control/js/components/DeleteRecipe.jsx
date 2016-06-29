eccf53d0106764be093f2baebd03f9dd2896165f
Brittany Storoz <brittanystoroz@gmail.com>
2016-06-29 15:03:47 -0400

    Move deleteRecipe into separate method

--- normandy/control/static/control/js/components/DeleteRecipe.jsx
+++ normandy/control/static/control/js/components/DeleteRecipe.jsx
@@ -4,8 +4,19 @@ import { makeApiRequest, recipeDeleted } from '../actions/ControlActions.js'
 import composeRecipeContainer from './RecipeContainer.jsx'
 
 class DeleteRecipe extends React.Component {
+  deleteRecipe(event) {
+    const { dispatch, recipeId } = this.props;
+
+    event.preventDefault();
+    dispatch(makeApiRequest('deleteRecipe', { recipeId }))
+    .then(response => {
+      dispatch(recipeDeleted(recipeId));
+      dispatch(push('/control/'));
+    });
+  }
+
   render() {
-    const { recipe, recipeId, dispatch } = this.props;
+    const { recipe, recipeId } = this.props;
     if (recipe) {
       return (
         <div className="fluid-7">
@@ -13,14 +24,7 @@ class DeleteRecipe extends React.Component {
             <p>Are you sure you want to delete "{recipe.name}"?</p>
             <div className="form-action-buttons">
               <div className="fluid-2 float-right">
-                <input type="submit" value="Confirm" class="delete" onClick={(e) => {
-                  e.preventDefault();
-                  dispatch(makeApiRequest('deleteRecipe', { recipeId }))
-                  .then(response => {
-                    dispatch(recipeDeleted(recipeId));
-                    dispatch(push('/control/'));
-                  });
-                }} />
+                <input type="submit" value="Confirm" class="delete" onClick={::this.deleteRecipe} />
               </div>
             </div>
           </form>
