5c757f0e0005aec28119d551cd765b194a781d53
Michael Kelly <mkelly@mozilla.com>
2016-07-01 10:38:27 -0700

    Pass engagementButtonLabel to showHeartbeat properly.

--- normandy/control/static/control/js/components/RecipePreview.js
+++ normandy/control/static/control/js/components/RecipePreview.js
@@ -2,6 +2,7 @@ import React, { PropTypes as pt } from 'react';
 import classNames from 'classnames';
 import composeRecipeContainer from './RecipeContainer.js';
 import { runRecipe } from '../../../../../selfrepair/static/js/self_repair_runner.js';
+import NormandyDriver from '../../../../../selfrepair/static/js/normandy_driver.js';
 
 class RecipePreview extends React.Component {
   propTypes = {
@@ -29,13 +30,15 @@ class RecipePreview extends React.Component {
   attemptPreview() {
     const { recipe } = this.props;
     const { recipeAttempted } = this.state;
+    const driver = new NormandyDriver();
+    driver.registerCallbacks();
 
     if (recipe && !recipeAttempted) {
       this.setState({
         recipeAttempted: true,
       });
 
-      runRecipe(recipe, { testing: true }).then(() => {
+      runRecipe(recipe, driver, { testing: true }).then(() => {
         this.setState({
           recipeExecuted: true,
         });
--- normandy/selfrepair/static/js/self_repair.js
+++ normandy/selfrepair/static/js/self_repair.js
@@ -1,16 +1,20 @@
-import Normandy from './normandy_driver.js';
+import Mozilla from './uitour.js';
+import NormandyDriver from './normandy_driver.js';
 import { fetchRecipes, filterContext, doesRecipeMatch, runRecipe } from './self_repair_runner.js';
+
+const driver = new NormandyDriver(Mozilla.UITour);
+driver.registerCallbacks();
 
 // Actually fetch and run the recipes.
 fetchRecipes().then(recipes => {
-  filterContext().then(context => {
+  filterContext(driver).then(context => {
     // Update Normandy driver with user's country.
-    Normandy._location.countryCode = context.normandy.country;
+    driver._location.countryCode = context.normandy.country;
 
     for (const recipe of recipes) {
       doesRecipeMatch(recipe, context).then(([, match]) => {
         if (match) {
-          runRecipe(recipe).catch(err => {
+          runRecipe(recipe, driver).catch(err => {
             console.error(err);
           });
         }
