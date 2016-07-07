b52b15a759a4d65d815fca8b2b0b591d3b7eb0cf
Mike Cooper <mythmon@gmail.com>
2016-07-07 16:10:13 -0700

    Fix errors introduced during linting
    Fixes bug 1271278.

--- normandy/control/tests/actions/controlActions.js
+++ normandy/control/tests/actions/controlActions.js
@@ -124,4 +124,21 @@ describe('controlApp Actions', () => {
       expect(fetchMock.calls('/api/v1/recipe/1/').length).toEqual(1);
     });
   });
+
+
+  describe('showNotification', () => {
+    it('automatically dismisses notifications after 10 seconds', async () => {
+      jasmine.clock().install();
+
+      const notification = { messageType: 'success', message: 'message' };
+      await store.dispatch(actionTypes.showNotification(notification));
+
+      const dismissAction = actionTypes.dismissNotification(notification.id);
+      expect(store.getActions()).not.toContain(dismissAction);
+      jasmine.clock().tick(10001);
+      expect(store.getActions()).toContain(dismissAction);
+
+      jasmine.clock().uninstall();
+    });
+  });
 });
