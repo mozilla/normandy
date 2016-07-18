064c62687a5be80fbaf32e909ac5f63b0019eae8
Michael Kelly <mkelly@mozilla.com>
2016-07-18 16:43:14 -0700

    Tests!

--- normandy/selfrepair/tests/test_self_repair_runner.js
+++ normandy/selfrepair/tests/test_self_repair_runner.js
@@ -1,6 +1,11 @@
 import fetchMock from 'fetch-mock';
+import { mockNormandy } from '../../recipes/tests/actions/utils.js';
 
-import { classifyClient } from '../static/js/self_repair_runner.js';
+import { classifyClient, doesRecipeMatch, filterContext } from '../static/js/self_repair_runner.js';
+
+
+const UUID_ISH_REGEX = /^[a-f0-9-]{36}$/;
+
 
 describe('Self-Repair Runner', () => {
   afterEach(() => {
@@ -22,6 +27,38 @@ describe('Self-Repair Runner', () => {
         country: 'US',
       }));
       expect(fetchMock.lastUrl()).toEqual(url);
+    });
+  });
+
+  describe('filterContext', () => {
+    it('should contain a valid user ID', async () => {
+      document.documentElement.dataset.classifyUrl = '/api/v1/classify/';
+      fetchMock.mock('/api/v1/classify/', 'GET', {
+        request_time: '2016-01-01',
+        country: 'US',
+      });
+
+      const driver = mockNormandy();
+      const context = await filterContext(driver);
+      expect(context.normandy.userId).toBeDefined();
+      expect(UUID_ISH_REGEX.test(context.normandy.userId)).toBe(true);
+    });
+  });
+
+  describe('doesRecipeMatch', () => {
+    it('should include the recipe in the filter expression context', async () => {
+      const recipe = {
+        filter_expression: 'recipe.shouldPass',
+        shouldPass: true,
+      };
+
+      let match = await doesRecipeMatch(recipe, {});
+      expect(match[1]).toBe(true);
+
+      // If shouldPass changes, so should the filter expression's result
+      recipe.shouldPass = false;
+      match = await doesRecipeMatch(recipe, {});
+      expect(match[1]).toBe(false);
     });
   });
 });
