df988220dc4c4be785cf3607a34f6496a31ecdbe
Mike Cooper <github@elem.us>
2016-07-21 15:03:17 -0700

    Feedback

--- contract-tests/conftest.py
+++ contract-tests/conftest.py
@@ -15,7 +15,7 @@ def pytest_addoption(parser):
         action="store_false",
         dest="verify",
         default=None,
-        help="Server to run tests against"
+        help="Don't verify SSL certs"
     )
 
 
@@ -24,21 +24,8 @@ def conf(request):
     return request.config
 
 
-class RequestVerifyWrapper(object):
-    """
-    `request.Session` doesn't have a way to permanently change the verify flag. This provides that.
-    """
-    def __init__(self, verify):
-        self.verify = verify
-        self.session = requests.Session()
-
-    def get(self, *args, **kwargs):
-        return self.session.get(*args, verify=self.verify, **kwargs)
-
-    # TODO: Add other HTTP methods as needed
-
-
 @pytest.fixture
 def requests_session(conf):
-    verify = conf.getoption('verify')
-    return RequestVerifyWrapper(verify)
+    session = requests.Session()
+    session.verify = conf.getoption('verify')
+    return session
