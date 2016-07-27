09eac9e169188a888ea03cce5e79023fb845dabf
Mike Cooper <github@elem.us>
2016-07-27 09:42:24 -0700

    Remove commented code in HeartbeatForm test

--- normandy/control/tests/components/action_forms/HeartbeatForm.js
+++ normandy/control/tests/components/action_forms/HeartbeatForm.js
@@ -1,12 +1,8 @@
 import React from 'react';
-// import { Provider } from 'react-redux';
 import { shallow } from 'enzyme';
-// import configureMockStore from 'redux-mock-store';
-// import thunk from 'redux-thunk';
 
 import HeartbeatForm from '../../../static/control/js/components/action_forms/HeartbeatForm.js';
 
-// const mockStore = configureMockStore([thunk]);
 
 describe('<HeartbeatForm>', () => {
   it('should work', () => {
