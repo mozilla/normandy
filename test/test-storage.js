941a155d9542237c11b6679a0a4340952a02e5fc
Mike Cooper <mythmon@gmail.com>
2016-10-18 17:00:59 -0700

    Rewrite driver Storage to use JSONFile.jsm

--- test/test-storage.js
+++ test/test-storage.js
@@ -7,7 +7,7 @@ const {promiseTest} = require('./utils.js');
 let store;
 
 exports['test set and get'] = promiseTest(assert => {
-  let store = new Storage('prefix');
+  let store = new Storage('prefix', {Promise});
 
   return store.setItem('key', 'value')
   .then(() => store.getItem('key'))
@@ -17,13 +17,13 @@ exports['test set and get'] = promiseTest(assert => {
 });
 
 exports["test value don't exist before set"] = promiseTest(assert => {
-  let store = new Storage('prefix');
+  let store = new Storage('prefix', {Promise});
   return store.getItem('absent')
   .then(value => assert.equal(value, null));
 });
 
 exports['test set and remove and get'] = promiseTest(assert => {
-  let store = new Storage('prefix');
+  let store = new Storage('prefix', {Promise});
 
   return store.setItem('removed', 'value')
   .then(() => store.removeItem('removed'))
@@ -32,7 +32,7 @@ exports['test set and remove and get'] = promiseTest(assert => {
 });
 
 exports['test tests are independent 1 of 2'] = promiseTest(assert => {
-  let store = new Storage('prefix');
+  let store = new Storage('prefix', {Promise});
   return store.getItem('counter')
   .then(value => store.setItem('counter', (value || 0) + 1))
   .then(() => store.getItem('counter'))
@@ -40,7 +40,7 @@ exports['test tests are independent 1 of 2'] = promiseTest(assert => {
 });
 
 exports['test tests are independent 2 of 2'] = promiseTest(assert => {
-  let store = new Storage('prefix');
+  let store = new Storage('prefix', {Promise});
   return store.getItem('counter')
   .then(value => store.setItem('counter', (value || 0) + 1))
   .then(() => store.getItem('counter'))
@@ -48,7 +48,7 @@ exports['test tests are independent 2 of 2'] = promiseTest(assert => {
 });
 
 before(exports, () => {
-  store = new Storage('prefix');
+  store = new Storage('prefix', {Promise});
 });
 
 after(exports, (name, assert, done) => {
