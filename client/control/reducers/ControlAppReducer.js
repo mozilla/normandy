853f734e53418330ee7aac20c7911a54291d0275
Andy Mikulski <amikulski@mozilla.com>
2016-11-14 19:14:58 -0700

    Simplified controlApp reducer

--- client/control/reducers/ControlAppReducer.js
+++ client/control/reducers/ControlAppReducer.js
@@ -1,14 +1,10 @@
 import {
-  REQUEST_IN_PROGRESS, REQUEST_COMPLETE, RECIPES_RECEIVED,
-  SINGLE_RECIPE_RECEIVED, RECIPE_ADDED, RECIPE_UPDATED, RECIPE_DELETED,
-  SET_SELECTED_RECIPE, SHOW_NOTIFICATION, DISMISS_NOTIFICATION,
+  REQUEST_IN_PROGRESS,
+  REQUEST_COMPLETE,
 } from 'actions/ControlActions.js';
 
 const initialState = {
-  recipes: null,
   isFetching: false,
-  selectedRecipe: null,
-  recipeListNeedsFetch: true,
   notifications: [],
 };
 
@@ -24,62 +20,6 @@ function controlAppReducer(state = initialState, action) {
       return {
         ...state,
         isFetching: false,
-      };
-
-    case RECIPES_RECEIVED:
-      return {
-        ...state,
-        recipes: action.recipes,
-        recipeListNeedsFetch: false,
-      };
-    case SINGLE_RECIPE_RECEIVED:
-      return {
-        ...state,
-        recipes: [action.recipe],
-        recipeListNeedsFetch: true,
-        selectedRecipe: action.recipe.id,
-      };
-
-    case SET_SELECTED_RECIPE:
-      return {
-        ...state,
-        selectedRecipe: action.recipeId,
-      };
-
-    case SHOW_NOTIFICATION:
-      return {
-        ...state,
-        notifications: [action.notification, ...state.notifications],
-      };
-
-    case DISMISS_NOTIFICATION:
-      return {
-        ...state,
-        notifications: state.notifications.filter(n => n.id !== action.notificationId),
-      };
-
-    case RECIPE_ADDED:
-      return {
-        ...state,
-        recipes: [
-          ...state.recipes || [],
-          action.recipe,
-        ],
-      };
-    case RECIPE_UPDATED:
-      return {
-        ...state,
-        recipes: state.recipes.map(recipe => {
-          if (recipe.id === action.recipe.id) {
-            return Object.assign(recipe, action.recipe);
-          }
-          return recipe;
-        }),
-      };
-    case RECIPE_DELETED:
-      return {
-        ...state,
-        recipes: state.recipes.filter(recipe => recipe.id !== action.recipeId),
       };
 
     default:
--- client/control/reducers/index.js
+++ client/control/reducers/index.js
@@ -6,9 +6,13 @@ import { routerReducer } from 'react-router-redux';
 
 // app reducer imports
 import controlAppReducer from './ControlAppReducer';
+import recipesReducer from './RecipesReducer';
+import notificationReducer from './NotificationReducer';
 
 export default combineReducers({
   controlApp: controlAppReducer,
+  recipes: recipesReducer,
+  notifications: notificationReducer,
   form: formReducer,
   routing: routerReducer,
 });
