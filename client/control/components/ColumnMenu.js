b8d3aaf658d6d9ed65dcc3eb4a18751572ca969d
Andy Mikulski <amikulski@mozilla.com>
2016-11-23 09:10:28 -0700

    Dumbify ColumnMenu component

--- client/control/components/ColumnMenu.js
+++ client/control/components/ColumnMenu.js
@@ -1,125 +1,36 @@
 import React, { PropTypes as pt } from 'react';
-import * as localForage from 'localforage';
 
 export default class ColumnMenu extends React.Component {
   static propTypes = {
-    onSelectionChange: pt.func.isRequired,
+    onInputChange: pt.func.isRequired,
+    columns: pt.array.isRequired,
   };
-
-  static defaultConfig = [{
-    label: 'Name',
-    value: 'name',
-    enabled: true,
-  }, {
-    label: 'Action Name',
-    value: 'action',
-    enabled: true,
-  }, {
-    label: 'Enabled',
-    value: 'enabled',
-    enabled: true,
-  }, {
-    label: 'Channels',
-    value: 'channels',
-  }, {
-    label: 'Locales',
-    value: 'locales',
-  }, {
-    label: 'Countries',
-    value: 'countries',
-  }, {
-    label: 'Start Time',
-    value: 'startTime',
-  }, {
-    label: 'End Time',
-    value: 'endTime',
-  }, {
-    label: 'Additional Filters',
-    value: 'additionalFilter',
-  }, {
-    label: 'Last Updated',
-    value: 'last_updated',
-    enabled: true,
-  }, {
-    label: 'Metadata',
-    value: 'metadata',
-    enabled: true,
-  }];
 
   constructor(props) {
     super(props);
 
-    this.state = {
-      data: ColumnMenu.defaultConfig,
-    };
+    this.state = {};
   }
 
-  componentDidMount() {
-    localForage.getItem('columns', (err, found) => {
-      if (found) {
-        this.setState({
-          data: found,
-        });
-      }
-
-      this.sendSelectionsToParent();
-    });
-  }
-
-  onInputChange(index) {
+  handleInputChange(index) {
     return evt => {
-      const newData = [].concat(this.state.data);
-      newData[index].enabled = evt.target.checked;
-
-      // update local state
-      this.setState({
-        data: newData,
-      });
-
-      // notify the parent that selection has changed
-      this.sendSelectionsToParent();
+      this.props.onInputChange(index, evt.target.checked);
     };
-  }
-
-  getSelectedOptions() {
-    const selected = [];
-    this.state.data.forEach(option => {
-      if (option.enabled) {
-        selected.push({
-          value: option.value,
-          label: option.label,
-        });
-      }
-    });
-
-    return selected;
-  }
-
-  sendSelectionsToParent() {
-    // we 'async' this because we need to pull the options from state,
-    // and in some cases we need state updates to resolve first
-    setTimeout(() => {
-      const options = this.getSelectedOptions();
-      this.props.onSelectionChange(options);
-
-      console.log('setting...', this.state.data);
-      localForage.setItem('columns', this.state.data);
-    }, 1);
   }
 
   render() {
-    const { data } = this.state;
+    const { columns } = this.props;
     return (
       <ul>
         {
-          data.map((option, index) =>
+          (columns).map((option, index) =>
             <li key={option.value + index}>
               <label htmlFor={option.value}>
                 <input
                   name={option.value}
                   type="checkbox"
                   checked={option.enabled}
-                  onChange={this.onInputChange(index)}
+                  onChange={this.handleInputChange(index)}
                 />
                 <span>
                   { option.label }
