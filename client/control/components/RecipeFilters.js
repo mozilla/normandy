58a10def1a667a737225625aa6154e55adb869ff
Andy Mikulski <amikulski@mozilla.com>
2016-11-23 09:11:36 -0700

    Add column config, localForage to RecipeFilters

--- client/control/components/RecipeFilters.js
+++ client/control/components/RecipeFilters.js
@@ -1,5 +1,6 @@
-/* eslint-disable import/no-named-as-default */
 import React, { PropTypes as pt } from 'react';
+import * as localForage from 'localforage';
+
 import SwitchFilter from './SwitchFilter.js';
 import ColumnMenu from './ColumnMenu.js';
 
@@ -12,17 +13,90 @@ export default class RecipeFilters extends React.Component {
     onFilterChange: pt.func.isRequired,
   };
 
+  static defaultColumnConfig = [{
+    label: 'Name',
+    value: 'name',
+    enabled: true,
+  }, {
+    label: 'Action Name',
+    value: 'action',
+    enabled: true,
+  }, {
+    label: 'Enabled',
+    value: 'enabled',
+    enabled: true,
+  }, {
+    label: 'Channels',
+    value: 'channels',
+  }, {
+    label: 'Locales',
+    value: 'locales',
+  }, {
+    label: 'Countries',
+    value: 'countries',
+  }, {
+    label: 'Start Time',
+    value: 'startTime',
+  }, {
+    label: 'End Time',
+    value: 'endTime',
+  }, {
+    label: 'Additional Filters',
+    value: 'additionalFilter',
+  }, {
+    label: 'Last Updated',
+    value: 'last_updated',
+    enabled: true,
+  }, {
+    label: 'Metadata',
+    value: 'metadata',
+    enabled: true,
+  }];
+
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      columns: [],
+    };
 
     this.handleFilterChange = ::this.handleFilterChange;
+    this.handleColumnInput = ::this.handleColumnInput;
+
+    this.handleFilterChange();
   }
 
-  handleFilterChange(filters) {
-    if (this.props.onFilterChange) {
-      this.props.onFilterChange(filters);
-    }
+  componentWillMount() {
+    localForage.getItem('columns', (err, found) => {
+      this.setState({
+        columns: found || RecipeFilters.defaultColumnConfig,
+      });
+      this.handleFilterChange();
+    });
+  }
+
+  handleFilterChange() {
+    const selected = [];
+    this.state.columns.forEach(col => {
+      if (col.enabled === true) {
+        selected.push(col);
+      }
+    });
+
+    localForage.setItem('columns', this.state.columns);
+
+    this.props.onFilterChange(selected);
+  }
+
+  handleColumnInput(columnIndex, isActive) {
+    const newColumns = [].concat(this.state.columns || []);
+    newColumns[columnIndex].enabled = isActive;
+
+    // update local state
+    this.setState({
+      columns: newColumns,
+    });
+
+    this.handleFilterChange();
   }
 
   render() {
@@ -57,7 +131,8 @@ export default class RecipeFilters extends React.Component {
         </div>
         <div className="fluid-8">
           <ColumnMenu
-            onSelectionChange={this.handleFilterChange}
+            columns={this.state.columns}
+            onInputChange={this.handleColumnInput}
           />
         </div>
       </div>
