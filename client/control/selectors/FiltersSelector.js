f0f9437e7cbfba9141c51b4bf2376b3cff5ac6c6
Andy Mikulski <amikulski@mozilla.com>
2016-11-29 09:36:20 -0700

    Correct/simplify filter selectors

--- client/control/selectors/FiltersSelector.js
+++ client/control/selectors/FiltersSelector.js
@@ -2,43 +2,17 @@ const isGroupSelected = group => {
   let enabled = false;
 
   if (group.selected) {
-    let enabledOptions = 0;
-
     group.options.forEach(option => {
       if (option.selected) {
-        enabledOptions += 1;
-
-        // if this group only allows one option,
-        if (!group.multiple) {
-          // we can declare this enabled
-          enabled = true;
-        } else {
-          // check if we hit the ceiling for options
-          enabled = enabledOptions >= group.options.length;
-        }
+        enabled = true;
       }
     });
   }
+
   return enabled;
 };
 
 export const getSelectedFilters = groups => groups.filter(isGroupSelected);
-// const getUnselectedFilters = groups => groups.filter(!isGroupSelected);
-
-export const getAvailableFilterOptions = groups => {
-  let newGroups = [].concat(groups || []);
-
-  newGroups = newGroups.map(group => {
-    if (group.selected && group.multiple) {
-      const newOptions = group.options.filter(option => !option.selected);
-      group.options = newOptions;
-    }
-
-    return group;
-  });
-
-  return newGroups;
-};
 
 export const getActiveFilters = groups =>
   [].concat(groups || [])
@@ -51,6 +25,17 @@ export const getActiveFilters = groups =>
       const activeOptions = [].concat(group.options).filter(option => option.selected);
       newGroup.options = activeOptions;
 
-      console.log('asdf', JSON.stringify(newGroup));
       return newGroup;
+    }).filter(x => x);
+
+
+export const getAvailableFilters = groups =>
+  [].concat(groups || [])
+    .map(group => {
+      const newGroup = { ...group };
+      // remove enabled filters
+      const activeOptions = [].concat(group.options).filter(option => !option.selected);
+      newGroup.options = activeOptions;
+
+      return newGroup.options.length === 0 ? null : newGroup;
     }).filter(x => x);
