e50f9471dbdfeaf1d3ba217311803965a9b7a0cd
Michael Kelly <mkelly@mozilla.com>
2017-01-12 14:56:47 -0800

    Use AWS sync command instead of rm and cp.

--- mock-recipe-server/bin/upload.sh
+++ mock-recipe-server/bin/upload.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 set -eu
 
+if [[ $# -ne 2 ]]; then
+    echo "Usage: $0 BUILD_DIR S3_BUCKET"
+    exit 1
+fi
+
 BUILD_DIR=$1
 S3_BUCKET=$2
 
-aws s3 rm --recursive "s3://$S3_BUCKET/"
-aws s3 cp --recursive "$BUILD_DIR" "s3://$S3_BUCKET/" --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+aws s3 sync --delete "$BUILD_DIR" "s3://$S3_BUCKET/" --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
