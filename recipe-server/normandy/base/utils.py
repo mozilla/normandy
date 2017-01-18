ff23e306fa6a3f6f033a7c3e9cd2d307d1ddee54
Rehan Dalal <rehandalal@gmail.com>
2017-01-18 04:41:08 -0500

    Feedback and tests

--- recipe-server/normandy/base/utils.py
+++ recipe-server/normandy/base/utils.py
@@ -55,7 +55,14 @@ def canonical_json_dumps(data):
 
 
 def filter_m2m(qs, field, values):
-    qs.annotate(_count=Count(field)).filter(_count=len(values))
+    """
+    Filters a queryset by an exact list of many to many relations.
+    """
+    values = list(values)
 
-    for value in values:
-        qs.filter(**{field: value})
+    qs = qs.annotate(_count=Count(field)).filter(_count=len(values))
+
+    if len(values):
+        qs = qs.filter(**{'{}__in'.format(field): values})
+
+    return qs
