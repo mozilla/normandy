6f26e5c78d5e614fb49785f8e7abaae2f92a4300
Michael Kelly <mkelly@mozilla.com>
2017-01-23 16:48:17 -0800

    Fix #444: Alias global object to window in recipe-client sandboxes.

--- recipe-client-addon/test/browser/browser_RecipeRunner.js
+++ recipe-client-addon/test/browser/browser_RecipeRunner.js
@@ -3,8 +3,9 @@
 const {utils: Cu} = Components;
 Cu.import("resource://shield-recipe-client/lib/RecipeRunner.jsm", this);
 
-add_task(function*() {
-  // Test that RecipeRunner can execute a basic recipe/action.
+add_task(function* execute() {
+  // Test that RecipeRunner can execute a basic recipe/action and return
+  // the result of execute.
   const recipe = {
     foo: "bar",
   };
@@ -16,7 +17,7 @@ add_task(function*() {
 
       execute() {
         return new Promise(resolve => {
-          resolve(this.recipe.foo);
+          resolve({foo: this.recipe.foo});
         });
       }
     }
@@ -25,5 +26,62 @@ add_task(function*() {
   `;
 
   const result = yield RecipeRunner.executeAction(recipe, {}, actionScript);
-  is(result, "bar", "Recipe executed correctly");
+  is(result.foo, "bar", "Recipe executed correctly");
+});
+
+add_task(function* error() {
+  // Test that RecipeRunner rejects with error messages from within the
+  // sandbox.
+  const actionScript = `
+    class TestAction {
+      execute() {
+        return new Promise((resolve, reject) => {
+          reject(new Error("ERROR MESSAGE"));
+        });
+      }
+    }
+
+    registerAction('test-action', TestAction);
+  `;
+
+  let gotException = false;
+  try {
+    yield RecipeRunner.executeAction({}, {}, actionScript);
+  } catch (err) {
+    gotException = true;
+    is(err.message, "ERROR MESSAGE", "RecipeRunner throws errors from the sandbox correctly.");
+  }
+  ok(gotException, "RecipeRunner threw an error from the sandbox.");
+});
+
+add_task(function* globalObject() {
+  // Test that window is an alias for the global object, and that it
+  // has some expected functions available on it.
+  const actionScript = `
+    window.setOnWindow = "set";
+    this.setOnGlobal = "set";
+
+    class TestAction {
+      execute() {
+        return new Promise(resolve => {
+          resolve({
+            setOnWindow: setOnWindow,
+            setOnGlobal: window.setOnGlobal,
+            setTimeoutExists: setTimeout !== undefined,
+            clearTimeoutExists: clearTimeout !== undefined,
+          });
+        });
+      }
+    }
+
+    registerAction('test-action', TestAction);
+  `;
+
+  const result = yield RecipeRunner.executeAction({}, {}, actionScript);
+  Assert.deepEqual(result, {
+    setOnWindow: "set",
+    setOnGlobal: "set",
+    setTimeoutExists: true,
+    clearTimeoutExists: true,
+  }, "sandbox.window is the global object and has expected functions.");
 });
