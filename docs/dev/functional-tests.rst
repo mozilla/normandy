469c3227edc0409f18525b5f09629936557ef740
Michael Kelly <mkelly@mozilla.com>
2017-02-07 11:26:46 -0800

    Always flush and re-init database when running functional tests.

--- docs/dev/functional-tests.rst
+++ docs/dev/functional-tests.rst
@@ -15,16 +15,7 @@ Prerequisites
 
 Generating Files
 ----------------
-1. If this is your first time running the tests, you must run the setup script
-   to create the docker containers used for running the tests:
-
-   .. code-block:: bash
-
-      # From the root of the repo
-      cd functional_tests
-      ./bin/setup.sh
-
-2. Run the ``runtests.sh`` file, passing in the path where you wish to save the
+1. Run the ``runtests.sh`` file, passing in the path where you wish to save the
    XML test report, and the path you wish to save the HTML test results:
 
    .. code-block:: bash
--- functional_tests/bin/runtests.sh
+++ functional_tests/bin/runtests.sh
@@ -24,5 +24,18 @@ export TEST_ARTIFACTS=$2
 
 # Run tests
 echo "Running functional tests"
+
+# Flush and re-initialize the database
+pushd "$REPO_DIR/compose"
+compose run -e CHECK_PORT=5432 -e CHECK_HOST=database takis # Wait for database
+compose run normandy ./manage.py flush --no-input
+compose run normandy ./manage.py migrate --no-input
+compose run normandy ./manage.py update_actions
+compose run normandy ./manage.py update_product_details
+compose run normandy ./manage.py initial_data
+compose run normandy ./bin/create_test_admin.py
+popd
+
+# Run the tests
 compose run -e CHECK_PORT=8000 -e CHECK_HOST=normandy takis # Wait for server
 compose run functionaltestrunner py.test --html=/test_artifacts/report.html --junitxml=/test_reports/pytest.xml
