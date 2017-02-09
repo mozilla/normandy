9aebecda4af23261221c85a290520b002e45a649
Mike Cooper <mythmon@gmail.com>
2017-02-09 09:30:43 -0800

    recipe-client-addon: test and build xpi in TC

---
+++ recipe-client-addon/bin/make-xpi.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -eu
+
+BASE_DIR="$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)")"
+TMP_DIR=$(mktemp -d)
+DEST="${TMP_DIR}/shield-recipe-client"
+
+mkdir -p $DEST
+
+# deletes the temp directory
+function cleanup {
+  rm -rf "$TMP_DIR"
+}
+trap cleanup EXIT
+
+while read -r LINE || [[ -n "${LINE}" ]]; do
+  mkdir -p "$(dirname "${DEST}/${LINE}")"
+  cp -r "${BASE_DIR}/${LINE}" "$(dirname "${DEST}/${LINE}")"
+done < "${BASE_DIR}/build-includes.txt"
+
+rm "${DEST}/install.rdf.in"
+cp "${BASE_DIR}/install.rdf" "${DEST}"
+
+pushd $DEST
+zip -r shield-recipe-client.xpi *
+mv shield-recipe-client.xpi $BASE_DIR
+popd
