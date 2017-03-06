ed5f4a32d600b650156f7af2ab19b10c248a4159
Michael Kelly <mkelly@mozilla.com>
2017-03-06 15:20:05 -0800

    Remove unused storage.py file.

--- recipe-server/normandy/storage.py
+++
@@ -1,6 +0,0 @@
-from pipeline.storage import PipelineMixin
-from whitenoise.django import GzipManifestStaticFilesStorage
-
-
-class GzipManifestPipelineStorage(PipelineMixin, GzipManifestStaticFilesStorage):
-    pass
