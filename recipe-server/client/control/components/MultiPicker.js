1f0f05a0e0a2fd8180986188495a1f0dc15588c5
Andy Mikulski <amikulski@mozilla.com>
2017-03-16 18:02:39 -0600

    Abstracted MultiPicker pieces

--- recipe-server/client/control/components/MultiPicker.js
+++ recipe-server/client/control/components/MultiPicker.js
@@ -1,6 +1,225 @@
 import React, { PropTypes as pt } from 'react';
 import { propTypes as reduxFormPropTypes } from 'redux-form';
+import cx from 'classnames';
 
+/**
+ * Abstracted piece of the multipicker - contains the actual select element and
+ * the text field that allows filtering of that list. Fires props upon selection change.
+ */
+class PickerControl extends React.Component {
+  static propTypes = {
+    options: pt.array.isRequired,
+    onSubmit: pt.func.isRequired,
+    titleLabel: pt.string.isRequired,
+    buttonLabel: pt.string.isRequired,
+    noneLabel: pt.string.isRequired,
+    searchLabel: pt.string.isRequired,
+    className: pt.string,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      // `filterText` is the user-inputted string to filter the list options by
+      filterText: '',
+      // `selected` is the existing options that the user currently has
+      selected: [],
+    };
+    this.handleTextChange = ::this.handleTextChange;
+    this.handleSelectUpdate = ::this.handleSelectUpdate;
+    this.handleConfirmation = ::this.handleConfirmation;
+    this.handleRef = ::this.handleRef;
+    this.renderSelectOption = ::this.renderSelectOption;
+  }
+
+  /**
+   * Collect and return an array of selected values for this PickerControl.
+   *
+   * @return {Array<string>}    Array of selected option values
+   */
+  getCurrentSelection() {
+    return Array.from(this.selectRef.selectedOptions).map(option => option.value).filter(x => x);
+  }
+
+  /**
+   * Given an array of options and some search text, returns an array of items that
+   * contain the search text in their value/label. This is used to filter out
+   * items in the <select> element based on the user's inputted search text.
+   *
+   * @param  {Array<{value: string, label: string }>} options Available options
+   * @param  {String} search  Text to search over options with
+   * @return {Array<Object>}  Array of available options that meet search criteria
+   */
+  getFilteredOptions(options, search) {
+    let newOptions = [].concat(options);
+
+    // There may not even be a search, so check that first.
+    if (search) {
+      // Lowercase-ify the search value to remove case sensitivity
+      const filterValue = search.toLowerCase();
+
+      // For each option, create a string value to search over, and then determine
+      // if that value contains our search text.
+      newOptions = newOptions.filter(({ value, label }) => {
+        const searchValues = [value, label].join('   ').toLowerCase();
+        return searchValues.indexOf(filterValue) > -1;
+      });
+    }
+
+    // newOptions will either be existing options, or filtered options (if search
+    // text exists)
+    return newOptions;
+  }
+
+  /**
+   * Gathers selected options and reports them as a comma-delineated string to
+   * the parent component. This is fired when user has made a selection and then
+   * decided to.. do something with it.
+   *
+   * @return {String} Comma-separated string of selected option values
+   */
+  handleConfirmation() {
+    this.props.onSubmit(this.getCurrentSelection());
+
+    // Clear the internal selection memory
+    this.setState({
+      selected: [],
+    });
+
+    // Clear user selection (otherwise it will remain in place, even after list
+    // children are added/removed)
+    this.selectRef.value = null;
+  }
+
+  /**
+   * Updates internal `selected` state with currently selected options, which is
+   * later used for things such as disabling the submit button.
+   */
+  handleSelectUpdate() {
+    this.setState({
+      selected: this.getCurrentSelection(),
+    });
+  }
+
+  /**
+   * Updates internal `filterText` state with user-inputed text value, which is
+   * later used for filtering displayed options.
+   *
+   * @param  {Event}  event   onChange event object
+   */
+  handleTextChange(event) {
+    this.setState({
+      filterText: event.target.value,
+    });
+  }
+
+  /**
+   * <select>'s ref handler. Simply stores a reference to this component's select
+   * element.
+   *
+   * @param  {Element} ref  Element for this <select>
+   */
+  handleRef(ref) {
+    this.selectedRef = ref;
+  }
+
+  /**
+   * Given an option object, renders a correlating <option> element.
+   *
+   * @param  {Object<{ label: string, value: string }>} option
+   * @param  {any} key    Value to use for `key` prop on this element
+   * @return {Element}    Compiled <option> element
+   */
+  renderSelectOption(option, key) {
+    return (
+      <option
+        key={key}
+        title={option.value}
+        value={option.value}
+      >
+        {option.label}
+      </option>
+    );
+  }
+
+  /**
+   * Returns a "no results found" or "no items selected" message. This is
+   * primarily used when the user is actively filtering items, or as the general
+   * 'empty list' state.
+   *
+   * @return {Element}  <option> element containing 'empty' message
+   */
+  renderEmptyMessage() {
+    const {
+      noneLabel,
+    } = this.props;
+
+    const {
+      filterText,
+    } = this.state;
+
+    return (
+      <option disabled className="option-label">
+       {filterText ? `No results found for "${filterText}"` : noneLabel}
+      </option>
+    );
+  }
+
+
+  render() {
+    const {
+      filterText,
+      selected,
+    } = this.state;
+
+    const {
+      buttonLabel,
+      className,
+      options,
+      searchLabel,
+      titleLabel,
+    } = this.props;
+
+    const filteredOptions = this.getFilteredOptions(options, filterText);
+    const frameClass = cx('mp-frame', className);
+
+    return (
+      <div className={frameClass}>
+        {titleLabel}
+
+        <input
+          type="search"
+          placeholder={searchLabel}
+          onChange={this.handleTextChange}
+        />
+
+        <select
+          ref={this.handleRef}
+          onChange={this.handleSelectUpdate}
+          multiple
+        >
+          { filteredOptions.map(this.renderSelectOption) }
+          { !filteredOptions.length && this.renderEmptyMessage() }
+        </select>
+
+        <button
+          disabled={selected.length <= 0}
+          type="button"
+          onClick={this.handleConfirmation}
+        >
+          { buttonLabel }
+        </button>
+      </div>
+    );
+  }
+}
+
+
+/**
+ * MultiPicker component - allows user to select multiple options and group them
+ * together into a separate list. Provides redux-form props (such as `onChange`)
+ * to work within forms.
+ */
 export default class MultiPicker extends React.Component {
   static propTypes = {
     unit: pt.string.isRequired,
@@ -9,96 +228,61 @@ export default class MultiPicker extends React.Component {
     onChange: reduxFormPropTypes.onChange,
   };
 
-  static getActiveSelectOptions = selectElement =>
-    Array.from(selectElement.selectedOptions).map(option => option.value).filter(x => x);
-
   constructor(props) {
     super(props);
-    this.state = {
-      filterText: null,
-    };
+    this.state = {};
 
-    this.handleChangeOption = ::this.handleChangeOption;
-    this.handleTextChange = ::this.handleTextChange;
-    this.getOptionFromValue = ::this.getOptionFromValue;
+    this.handleApplySelection = ::this.handleApplySelection;
+    this.handleRemoveSelection = ::this.handleRemoveSelection;
   }
 
   /**
-   * Given an option's value, finds the specified option from the given
-   * props.options list, and returns it.
+   * Event handler for a PickerControl that has fired its `onConfirm` prop.
+   * At this point, a user has selected options in one of the pickers, and wants
+   * to do something with it. In this case, they want to 'apply' the selection,
+   * which essentially means 'mark it as selected and move it to the other PickerControl'.
    *
-   * @param  {string} value Option's value
-   * @return {object}       Option object (has label, value)
+   * @param  {Array<string>}  selection   Array of selected values to apply
    */
-  getOptionFromValue(value) {
+  handleApplySelection(selection = []) {
     const {
-      options = [],
+      value = [],
     } = this.props;
 
-    return options.find(option => option.value === value);
+    // A redux-form value can be a string or an array, but we want an array.
+    const valueArray = typeof value === 'string' ? value.split(', ') : value;
+
+    // Get a set of unique selected values from existing values and those coming in
+    const uniqueSelections = new Set(valueArray.concat(selection));
+
+    // Convert the Set of unique values into an array.
+    const newEnabled = Array.from(uniqueSelections);
+
+    // Send that value to redux-form.
+    this.props.onChange(newEnabled.join(', '));
   }
 
-  getDisplayedOptions() {
+  /**
+   * Event handler for a PickerControl that has fired its `onConfirm` prop.
+   * At this point, a user has selected options in one of the pickers, and wants
+   * to do something with it. In this case, they want to 'remove' the selection,
+   * which essentially means 'mark it as NOT selected and move it to the first PickerControl'.
+   *
+   * @param  {Array<string>}  selection   Array of selected values to remove
+   */
+  handleRemoveSelection(selection = []) {
     const {
-      options = [],
-      value,
+      value = [],
     } = this.props;
 
-    const {
-      filterText,
-    } = this.state;
+    // A redux-form value can be a string or an array, but we want an array.
+    const valueArray = typeof value === 'string' ? value.split(', ') : value;
 
-    let selectedOptions = options.filter(option => value.indexOf(option.value) === -1);
+    // New enabled selections will be those remaining after filtering de-selections.
+    const newEnabled = valueArray.filter(val => selection.indexOf(val) === -1);
 
-    if (filterText) {
-      const searchText = filterText.toLowerCase();
-
-      selectedOptions = selectedOptions.filter(option => {
-        const searchValues = Object.values(option).join(' ').toLowerCase();
-        return searchValues.indexOf(searchText) > -1;
-      });
-    }
-
-    return selectedOptions;
-  }
-
-  handleTextChange(event) {
-    this.setState({
-      filterText: event.target.value,
-    });
-  }
-
-  handleChangeOption(type) {
-    return () => {
-      const ref = type === 'apply' ? this.availableRef : this.selectedRef;
-      if (!type || !ref) {
-        return;
-      }
-
-      const {
-        value = [],
-        onChange = () => {},
-      } = this.props;
-
-      // Get a list of all selected values.
-      let selection = [].concat(value);
-      // Pick up list of selected options from the dom ref
-      const refSelected = MultiPicker.getActiveSelectOptions(ref);
-
-      // Apply = take from the 'available' section and put in 'selected'
-      if (type === 'apply') {
-        selection = selection.concat(refSelected);
-      // Remove = take from 'selected' and put back in 'available'
-      } else if (type === 'remove') {
-        selection = selection.filter(val => refSelected.indexOf(val) === -1);
-      }
-
-      // Clear user input.
-      ref.value = null;
-
-      // Report the updated selected filters to the parent.
-      onChange(selection.join(','));
-    };
+    // Send that value to redux-form.
+    this.props.onChange(newEnabled.join(', '));
   }
 
   /**
@@ -106,84 +290,40 @@ export default class MultiPicker extends React.Component {
    */
   render() {
     const {
-      unit,
+      unit = '',
       value,
+      options,
     } = this.props;
 
-    let pickerValue = value || [];
+    const lowercaseUnit = unit.toLowerCase();
 
-    // Value can be a string or an array, but we want an array.
-    if (value && typeof value === 'string') {
-      pickerValue = pickerValue.split(',');
-    }
-
-    // We're given a comma-separated list of strings as values,
-    // so we'll convert those values into their respective `option`s here
-    pickerValue = pickerValue.map(this.getOptionFromValue);
-
-    const displayedOptions = this.getDisplayedOptions();
+    // `value` is the currently selected value for the component from redux-form.
+    // We can compare the given `options` with the `value` to determine what has been selected
+    // by the user already or not, and then populate the displayed lists accordingly.
+    const availableOptions = options.filter(option => value.indexOf(option.value) === -1);
+    const selectedOptions = options.filter(option => value.indexOf(option.value) !== -1);
 
     return (
       <div className="multipicker">
+        <PickerControl
+          options={availableOptions}
+          className="mp-from"
+          titleLabel={`Available ${unit}`}
+          searchLabel={`Filter Available ${unit}`}
+          onSubmit={this.handleApplySelection}
+          buttonLabel={`Add ${unit}`}
+          noneLabel={`No ${lowercaseUnit} available.`}
+        />
 
-        <div className="mp-frame mp-from">
-          {`Available ${unit}`}
-
-          <input
-            type="search"
-            placeholder={`Search ${unit}`}
-            onChange={this.handleTextChange}
-          />
-
-          <select ref={ref => { this.availableRef = ref; }} multiple>
-            {
-              (displayedOptions).map((option, idx) =>
-                <option
-                  key={idx}
-                  title={option.value}
-                  value={option.value}
-                >
-                  {option.label}
-                </option>
-              )
-            }
-          </select>
-        </div>
-
-        {
-          pickerValue &&
-            <div className="mp-button-group">
-              <button
-                onClick={this.handleChangeOption('remove')}
-                type="button"
-              >
-                ⇜
-              </button>
-              <button
-                onClick={this.handleChangeOption('apply')}
-                type="button"
-              >
-                ⇝
-              </button>
-            </div>
-        }
-
-        <div className="mp-frame mp-to">
-          {`Selected ${unit}`}
-          <select ref={ref => { this.selectedRef = ref; }} multiple>
-            {
-              pickerValue.map((option, idx) =>
-                <option
-                  key={idx}
-                  title={option.value}
-                  value={option.value}
-                >
-                  {option.label}
-                </option>
-              )
-            }
-          </select>
-        </div>
+        <PickerControl
+          options={selectedOptions}
+          className="mp-to"
+          titleLabel={`Selected ${unit}`}
+          searchLabel={`Filter Selected ${unit}`}
+          onSubmit={this.handleRemoveSelection}
+          buttonLabel={`Remove ${unit}`}
+          noneLabel={`No ${lowercaseUnit} selected.`}
+        />
       </div>
     );
   }
--- recipe-server/client/control/tests/components/test_MultiPicker.js
+++ recipe-server/client/control/tests/components/test_MultiPicker.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import MultiPicker from 'control/components/MultiPicker';
 
@@ -17,52 +16,5 @@ describe('<MultiPicker>', () => {
   it('should render without throwing', () => {
     const wrapper = () => shallow(<MultiPicker {...propFactory()} />);
     expect(wrapper).not.toThrow();
-  });
-
-  describe('getActiveSelectOptions', () => {
-    it('should return an empty array if no selections are made', () => {
-      const onRender = select =>
-        expect(MultiPicker.getActiveSelectOptions(select)).toEqual([]);
-
-      ReactDOM.render(
-        <select ref={onRender}>
-          <option value="">Select One</option>
-          <option value="1">Option 1</option>
-          <option value="2">Option 2</option>
-          <option value="3">Option 3</option>
-        </select>,
-        document.createElement('div')
-      );
-    });
-
-    it('should return an array of selections are made', () => {
-      const onRender = select =>
-        expect(MultiPicker.getActiveSelectOptions(select)).toEqual(['2']);
-
-      ReactDOM.render(
-        <select ref={onRender}>
-          <option value="">Select One</option>
-          <option value="1">Option 1</option>
-          <option value="2" selected>Option 2</option>
-          <option value="3">Option 3</option>
-        </select>,
-        document.createElement('div')
-      );
-    });
-
-    it('should handle multiple selections', () => {
-      const onRender = select =>
-        expect(MultiPicker.getActiveSelectOptions(select)).toEqual(['2', '3']);
-
-      ReactDOM.render(
-        <select multiple ref={onRender}>
-          <option value="">Select One</option>
-          <option value="1">Option 1</option>
-          <option value="2" selected>Option 2</option>
-          <option value="3" selected>Option 3</option>
-        </select>,
-        document.createElement('div')
-      );
-    });
   });
 });
