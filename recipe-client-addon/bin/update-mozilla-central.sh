27cf86854d8557754ac3265bfeab44165ccb5c35
Mike Cooper <mythmon@gmail.com>
2017-03-30 15:16:24 -0700

    recipe-client-addon: Calculate add-on versions automatically from git tags

--- recipe-client-addon/bin/update-mozilla-central.sh
+++ recipe-client-addon/bin/update-mozilla-central.sh
@@ -8,12 +8,17 @@ if [[ $# -lt 1 ]]; then
 fi
 
 baseDir="$(dirname "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )")"
-mozCentral="$1"
+mozCentral=$(readlink -f "$1")
 dest="${mozCentral}/browser/extensions/shield-recipe-client"
 
+cd "$baseDir"
 rm -rf "${dest}"/*
 
 while read -r line || [[ -n "${line}" ]]; do
   mkdir -p "$(dirname "${dest}/${line}")"
   cp -r "${baseDir}/${line}" "$(dirname "${dest}/${line}")"
 done < "${baseDir}/build-includes.txt"
+
+# This produces versions like "45", "45.5.abcdef", and "45.5.abcdef.dirty"
+version=$(git describe --dirty | sed -e 's/^v//' -e 's/-/./g')
+sed -i -e "s/@NORMANDY_VERSION@/${version}/" "${dest}/install.rdf.in"
