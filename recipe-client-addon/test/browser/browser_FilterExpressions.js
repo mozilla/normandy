b07cf3e891f9fe3be7a9bcbf38779f532b42173b
Andy Mikulski <amikulski@mozilla.com>
2017-04-05 09:39:23 -0600

    Add negative preference tests

--- recipe-client-addon/test/browser/browser_FilterExpressions.js
+++ recipe-client-addon/test/browser/browser_FilterExpressions.js
@@ -1,6 +1,5 @@
 "use strict";
 
-Cu.import("resource://gre/modules/Preferences.jsm");
 Cu.import("resource://shield-recipe-client/lib/FilterExpressions.jsm", this);
 
 // Basic JEXL tests
@@ -44,23 +43,23 @@ add_task(async function() {
   let val;
   // Test stable sample returns true for matching samples
   val = await FilterExpressions.eval('["test"]|stableSample(1)');
-  is(val, true, "Stable sample returns true for 100% sample");
+  ok(val, "Stable sample returns true for 100% sample");
 
   // Test stable sample returns true for matching samples
   val = await FilterExpressions.eval('["test"]|stableSample(0)');
-  is(val, false, "Stable sample returns false for 0% sample");
+  ok(!val, "Stable sample returns false for 0% sample");
 
   // Test stable sample for known samples
   val = await FilterExpressions.eval('["test-1"]|stableSample(0.5)');
-  is(val, true, "Stable sample returns true for a known sample");
+  ok(val, "Stable sample returns true for a known sample");
   val = await FilterExpressions.eval('["test-4"]|stableSample(0.5)');
-  is(val, false, "Stable sample returns false for a known sample");
+  ok(!val, "Stable sample returns false for a known sample");
 
   // Test bucket sample for known samples
   val = await FilterExpressions.eval('["test-1"]|bucketSample(0, 5, 10)');
-  is(val, true, "Bucket sample returns true for a known sample");
+  ok(val, "Bucket sample returns true for a known sample");
   val = await FilterExpressions.eval('["test-4"]|bucketSample(0, 5, 10)');
-  is(val, false, "Bucket sample returns false for a known sample");
+  ok(!val, "Bucket sample returns false for a known sample");
 });
 
 // Preference tests
@@ -69,24 +68,26 @@ add_task(async function() {
   // Compare the value of the preference
   await SpecialPowers.pushPrefEnv({set: [["normandy.test.value", 3]]});
   val = await FilterExpressions.eval('"normandy.test.value"|preferenceValue == 3');
-  is(val, true, "preferenceValue expression compares against preference values");
+  ok(val, "preferenceValue expression compares against preference values");
+  val = await FilterExpressions.eval('"normandy.test.value"|preferenceValue == "test"');
+  ok(!val, "preferenceValue expression fails value checks appropriately");
 
   // preferenceValue can take a default value as an optional argument, which
   // defaults to `undefined`.
   val = await FilterExpressions.eval('"normandy.test.default"|preferenceValue(false) == false');
-  is(val, true, `preferenceValue takes optional 'default value' param for prefs without set values`);
-
-  val = await FilterExpressions.eval('"normandy.test.value"|preferenceValue(5) == 3');
-  is(val, true, `preferenceValue default param is not returned for prefs with set values`);
+  ok(val, `preferenceValue takes optional 'default value' param for prefs without set values`);
+  val = await FilterExpressions.eval('"normandy.test.value"|preferenceValue(5) == 5');
+  ok(!val, `preferenceValue default param is not returned for prefs with set values`);
 
   // Compare if the preference is user set
-  val = await FilterExpressions.eval('"normandy.test.isSet"|preferenceIsUserSet != true');
-  is(val, true, "preferenceIsUserSet expression determines if preference is even set");
-
+  val = await FilterExpressions.eval('"normandy.test.isSet"|preferenceIsUserSet == true');
+  ok(!val, "preferenceIsUserSet expression determines if preference is set at all");
   val = await FilterExpressions.eval('"normandy.test.value"|preferenceIsUserSet == true');
-  is(val, true, "preferenceIsUserSet expression determines if user set preference themselves");
+  ok(val, "preferenceIsUserSet expression determines if user's preference has been set");
 
   // Compare if the preference has _any_ value, whether it's user-set or default,
-  val = await FilterExpressions.eval('"normandy.test.nonexistant"|preferenceExists == false');
-  is(val, true, "preferenceExists expression determines if preference exists at all");
+  val = await FilterExpressions.eval('"normandy.test.nonexistant"|preferenceExists == true');
+  ok(!val, "preferenceExists expression determines if preference exists at all");
+  val = await FilterExpressions.eval('"normandy.test.value"|preferenceExists == true');
+  ok(val, "preferenceExists expression fails existence check appropriately");
 });
