95f3efecb3f19f2530c9acf4bdb774dea2fe7353
Michael Kelly <mkelly@mozilla.com>
2017-04-17 13:12:03 -0700

    Fetch tags from mozilla/normandy before syncing in TC fetch script.
    TaskCluster uses the fork from a PR as the Git repo as the remote for
    the checkout in TC jobs, which means that tags from the mozilla source
    repo are not normally available. By adding and fetching the mozilla
    repo to the checkout, we fetch the tags, which allows the sync script
    to create a better version number for the add-on while it applies its
    changes to the gecko-dev checkout.

--- recipe-client-addon/bin/tc/fetch.sh
+++ recipe-client-addon/bin/tc/fetch.sh
@@ -5,6 +5,12 @@ set -eu
 echo 'Downloading gecko-dev...'
 curl --location --fail --silent --show-error https://github.com/mozilla/gecko-dev/archive/master.tar.gz | tar xz
 
+echo 'Pulling tags from mozilla/normandy repo on Github...'
+pushd normandy
+git remote add mozilla https://github.com/mozilla/normandy.git
+git fetch mozilla
+popd
+
 echo 'Syncing recipe-client-addon to gecko-dev...'
 pushd normandy/recipe-client-addon
 npm install
