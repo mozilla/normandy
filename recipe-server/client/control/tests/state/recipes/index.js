d7baf2356824a90ae7379838cfb1fa2750399f92
Rehan Dalal <rehandalal@gmail.com>
2017-05-05 14:25:40 -0400

    Fix JS tests

--- recipe-server/client/control/tests/state/recipes/index.js
+++ recipe-server/client/control/tests/state/recipes/index.js
@@ -10,6 +10,17 @@ export const INITIAL_STATE = {
 
 export const RECIPE = {
   id: 1,
+  action: {
+    id: 1,
+    name: 'test-action',
+  },
+  latest_revision: {
+    id: '9f86d081',
+    recipe: {
+      id: 1,
+    },
+  },
+  approved_revision: null,
 };
 
 export const FILTERS = {
