a3efe47ddac70aa93252df07d28007f2400c5185
Rehan Dalal <rehandalal@gmail.com>
2017-05-12 21:42:05 -0400

    Add additional tests

--- recipe-server/client/control/tests/state/approvalRequests/test_reducers.js
+++ recipe-server/client/control/tests/state/approvalRequests/test_reducers.js
@@ -1,6 +1,7 @@
 import { fromJS } from 'immutable';
 
 import {
+  APPROVAL_REQUEST_DELETE,
   APPROVAL_REQUEST_RECEIVE,
 } from 'control/state/action-types';
 import approvalRequestsReducer from 'control/state/approvalRequests/reducers';
@@ -24,5 +25,19 @@ describe('Approval requests reducer', () => {
       ...INITIAL_STATE,
       items: INITIAL_STATE.items.set(APPROVAL_REQUEST.id, fromJS(APPROVAL_REQUEST)),
     });
+  });
+
+  it('should handle APPROVAL_REQUEST_DELETE', () => {
+    const state = approvalRequestsReducer(undefined, {
+      type: APPROVAL_REQUEST_RECEIVE,
+      approvalRequest: APPROVAL_REQUEST,
+    });
+
+    const updatedState = approvalRequestsReducer(state, {
+      type: APPROVAL_REQUEST_DELETE,
+      approvalRequestId: APPROVAL_REQUEST.id,
+    });
+
+    expect(updatedState).toEqual(INITIAL_STATE);
   });
 });
--- recipe-server/client/control/tests/state/revisions/test_reducers.js
+++ recipe-server/client/control/tests/state/revisions/test_reducers.js
@@ -2,6 +2,7 @@ import { fromJS } from 'immutable';
 import * as matchers from 'jasmine-immutable-matchers';
 
 import {
+  RECIPE_DELETE,
   REVISION_RECEIVE,
 } from 'control/state/action-types';
 import revisionsReducer from 'control/state/revisions/reducers';
@@ -40,5 +41,19 @@ describe('Revisions reducer', () => {
     expect(updatedState.items).toEqualImmutable(
       INITIAL_STATE.items.set(REVISION.id, fromJS(reducedRevision))
     );
+  });
+
+  it('should handle RECIPE_DELETE', () => {
+    const state = revisionsReducer(undefined, {
+      type: REVISION_RECEIVE,
+      revision: REVISION,
+    });
+
+    const updatedState = revisionsReducer(state, {
+      type: RECIPE_DELETE,
+      recipeId: REVISION.recipe.id,
+    });
+
+    expect(updatedState).toEqual(INITIAL_STATE);
   });
 });
