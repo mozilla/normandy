ee294edc321b5c5b1cfdc7ac151b0f328345eec5
Andy Mikulski <amikulski@mozilla.com>
2017-06-28 14:21:51 -0700

    Replace elements with Cards

--- recipe-server/client/control_new/components/pages/Gateway.js
+++ recipe-server/client/control_new/components/pages/Gateway.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import { Link } from 'redux-little-router';
 
-import { Row, Col, Icon } from 'antd';
+import { Row, Col, Icon, Card } from 'antd';
 
 export default function Gateway() {
   return (
     <Row className="page-gateway" type="flex" justify="space-around" align="top">
       <Col className="gw-col" span={8}>
-        <h2 className="title">Recipes</h2>
-        <Icon type="book" />
-        <p>Basic SHIELD recipes</p>
-        <Link href="/recipes">Go to Recipes</Link>
+        <Card title="Recipes">
+          <Icon type="book" />
+          <p>Basic SHIELD recipes</p>
+          <Link href="/recipes">Go to Recipes</Link>
+        </Card>
       </Col>
       <Col className="gw-col" span={8}>
-        <h2 className="title">Extensions</h2>
-        <Icon type="api" />
-        <p>Deployed SHIELD WebExtensions</p>
-        <Link href="/extensions">Go to Extensions</Link>
+        <Card title="Extensions">
+          <Icon type="code" />
+          <p>SHIELD WebExtensions</p>
+          <Link href="/extensions">Go to Extensions</Link>
+        </Card>
       </Col>
     </Row>
   );
