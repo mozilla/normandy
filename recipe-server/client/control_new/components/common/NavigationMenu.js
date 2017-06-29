5220c3fb0fe69ca323db13a0eae55efc03af7fb7
Andy Mikulski <amikulski@mozilla.com>
2017-06-29 10:53:58 -0700

    Convert recursive menu to components

--- recipe-server/client/control_new/components/common/NavigationMenu.js
+++ recipe-server/client/control_new/components/common/NavigationMenu.js
@@ -6,73 +6,47 @@ import { Link } from 'redux-little-router';
 
 const { SubMenu, Item } = Menu;
 
-export class NavigationMenu extends React.Component {
-  static propTypes = {
-    router: PropTypes.object.isRequired,
-  };
+export function NavigationMenu({ router }) {
+  const { pathname, search } = router;
 
-  static menuList = {
-    Home: '/',
-    Recipes: {
-      'View All': '/recipes',
-      // These are just examples - later, the URLs will point to filtered listings.
-      Heartbeat: '/recipes?heartbeat',
-      'console-log': '/recipes?console-log',
-      'Preference Study': '/recipes?pref-exp',
-    },
-    Extensions: {
-      'View All': '/extensions',
-      'Preference Study': '/extensions?pref-exp',
-    },
-  };
+  return (
+    <Menu
+      defaultOpenKeys={['Recipes', 'Extensions']}
+      selectedKeys={[pathname + search]}
+      mode="inline"
+    >
+      <Item key="/"><Link href="/">Home</Link></Item>
 
-  /**
-   * Given an object of title:url's, recursively builds a tree navigation UI.
-   * Steps through each nested menu and creates `SubMenu`s if needed, otherwise
-   * creates an `Item` with a link inside of it.
-   *
-   * @param  {Object} menu       Menu definition object, can contain nested menus.
-   * @param  {String} namespace? Name of the current menu being explored.
-   * @return {Node}   Compiled Item or SubMenu node.
-   */
-  generateMenuContents(menu, namespace = '') {
-    return (Object.keys(menu).map(key => {
-      let item;
+      <SubMenu title="Recipes" key="Recipes">
+        <Item key="/recipes">
+          <Link href="/recipes">View All</Link>
+        </Item>
+        <Item key="/recipes?hearbeat">
+          <Link href="/recipes?hearbeat">Heartbeat</Link>
+        </Item>
+        <Item key="/recipes?console-log">
+          <Link href="/recipes?console-log">console-log</Link>
+        </Item>
+        <Item key="/recipes?pref-study">
+          <Link href="/recipes?pref-study">Preference Study</Link>
+        </Item>
+      </SubMenu>
 
-      // A string means it's a URL to the destination page.
-      if (typeof menu[key] === 'string') {
-        item = (
-          <Item key={menu[key]}>
-            <Link href={menu[key]}>{key}</Link>
-          </Item>
-        );
-      } else {
-      // An object means it's a set of nested menus/items.
-        item = (
-          <SubMenu title={key} key={key + namespace}>
-            { this.generateMenuContents(menu[key], key) }
-          </SubMenu>
-        );
-      }
-
-      return item;
-    }));
-  }
-
-  render() {
-    const { pathname, search } = this.props.router;
-
-    return (
-      <Menu
-        defaultOpenKeys={['Recipes', 'Extensions']}
-        selectedKeys={[pathname + search]}
-        mode="inline"
-      >
-        { this.generateMenuContents(NavigationMenu.menuList) }
-      </Menu>
-    );
-  }
+      <SubMenu title="Extensions" key="Extensions">
+        <Item key="/extensions">
+          <Link href="/extensions">View All</Link>
+        </Item>
+        <Item key="/extensions?pref-study">
+          <Link href="/extensions?pref-study">Preference Study</Link>
+        </Item>
+      </SubMenu>
+    </Menu>
+  );
 }
+
+NavigationMenu.propTypes = {
+  router: PropTypes.object.isRequired,
+};
 
 
 export default connect(
