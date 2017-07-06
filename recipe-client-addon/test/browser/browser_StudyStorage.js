0e79179ffd3eb6c8cf09f6b80bc736136dd0252d
Andy Mikulski <amikulski@mozilla.com>
2017-07-06 13:38:47 -0600

    Code review

--- recipe-client-addon/test/browser/browser_StudyStorage.js
+++ recipe-client-addon/test/browser/browser_StudyStorage.js
@@ -2,53 +2,32 @@
 
 Cu.import("resource://shield-recipe-client/lib/StudyStorage.jsm", this);
 
+function withStudyStorage(testFn) {
+  return async () => {
+    await testFn(StudyStorage);
+
+    await StudyStorage.clearAllData();
+    await StudyStorage.closeConnection();
+  };
+}
+
 // -------
 
-add_task(async function checkReset() {
-  ok(!StudyStorage.database, "StudyStorage starts without an IDB instance.");
-
-  await StudyStorage.getDatabase();
-  ok(StudyStorage.database, "StudyStorage creates an IDB instance.");
-
-  await StudyStorage.reset();
-  ok(!StudyStorage.database, "StudyStorage correctly removes its IDB instance.");
-});
-
-add_task(async function checkGetDatabase() {
-  await StudyStorage.reset(true);
-  ok(!StudyStorage.database, "StudyStorage starts without an IDB instance.");
-
-  const db1 = await StudyStorage.getDatabase();
-  ok(db1, "StudyStorage creates an IDB instance.");
-
-  const db2 = await StudyStorage.getDatabase();
-  is(db1, db2, "StudyStorage creates only one IDB instance at a time.");
-});
-
-
-add_task(async function checkSaveStudy() {
-  await StudyStorage.reset(true);
-  let success = true;
-
+add_task(withStudyStorage(async function checkSaveStudy(storage) {
   try {
-    await StudyStorage.saveStudyData({
+    await storage.saveStudy({
       name: "hi",
       version: 2,
       description: "fake",
       id: 12345
     });
   } catch (err) {
-    success = false;
     throw err;
   }
-
-  ok(success, "StudyStorage saved data successfully using `saveStudyData`.");
-});
+}));
 
 // Test looking up study data
-add_task(async function checkGetStudyData() {
-  await StudyStorage.reset(true);
-
+add_task(withStudyStorage(async function checkGetStudy(storage) {
   const mockStudy = {
     name: "Test",
     version: 2,
@@ -56,37 +35,36 @@ add_task(async function checkGetStudyData() {
     id: "12345"
   };
 
-  await StudyStorage.saveStudyData(mockStudy);
+  await storage.saveStudy(mockStudy);
 
-  const study = await StudyStorage.getStudyData("12345");
+  const study = await storage.getStudy("Test");
 
   is(study.name, mockStudy.name, "StudyStorage loaded name successfully using `getStudyData`.");
   is(study.version, mockStudy.version,
     "StudyStorage loaded version successfully using `getStudyData`.");
-});
+}));
 
 
 // Test saving different data with the same `id`
-add_task(async function checkOverwriteStudy() {
-  await StudyStorage.reset(true);
+add_task(withStudyStorage(async function checkUpdateStudy(storage) {
 
-  await StudyStorage.saveStudyData({
+  await storage.saveStudy({
     name: "hey",
     version: 1,
     description: "fake",
     id: "12345"
   });
 
-  await StudyStorage.saveStudyData({
-    name: "hello",
-    version: 2,
-    description: "fake",
+  await storage.updateStudy({
+    name: "hey",
+    version: 123,
+    description: "test",
     id: "12345"
   });
 
-  let savedStudy = await StudyStorage.getStudyData("12345");
+  const savedStudy = await storage.getStudy("hey");
 
   ok(savedStudy, "saveStudyData correctly saved the study.");
-  is(savedStudy.name, "hello", "saveStudyData correctly updated the study name.");
-  is(savedStudy.version, 2, "saveStudyData correctly updated the study version");
-});
+  is(savedStudy.name, "hey", "saveStudyData correctly updated the study name.");
+  is(savedStudy.version, 123, "saveStudyData correctly updated the study version");
+}));
