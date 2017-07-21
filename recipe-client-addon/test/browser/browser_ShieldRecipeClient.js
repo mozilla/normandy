4be862d723d1c20fad635befa753f1e902841fbc
Michael Kelly <mkelly@mozilla.com>
2017-07-21 12:58:46 -0700

    Properly mock about page initialization while testing startup functions.

--- recipe-client-addon/test/browser/browser_ShieldRecipeClient.js
+++ recipe-client-addon/test/browser/browser_ShieldRecipeClient.js
@@ -1,5 +1,6 @@
 "use strict";
 
+Cu.import("resource://shield-recipe-client/lib/AboutPages.jsm", this);
 Cu.import("resource://shield-recipe-client/lib/ShieldRecipeClient.jsm", this);
 Cu.import("resource://shield-recipe-client/lib/RecipeRunner.jsm", this);
 Cu.import("resource://shield-recipe-client/lib/PreferenceExperiments.jsm", this);
@@ -7,24 +8,46 @@ Cu.import("resource://shield-recipe-client/lib/PreferenceExperiments.jsm", this)
 add_task(async function testStartup() {
   sinon.stub(RecipeRunner, "init");
   sinon.stub(PreferenceExperiments, "init");
+  sinon.stub(AboutPages, "init");
 
   await ShieldRecipeClient.startup();
+  ok(AboutPages.init.called, "startup calls AboutPages.init");
   ok(PreferenceExperiments.init.called, "startup calls PreferenceExperiments.init");
   ok(RecipeRunner.init.called, "startup calls RecipeRunner.init");
 
   PreferenceExperiments.init.restore();
   RecipeRunner.init.restore();
+  AboutPages.init.restore();
 });
 
 add_task(async function testStartupPrefInitFail() {
   sinon.stub(RecipeRunner, "init");
   sinon.stub(PreferenceExperiments, "init").returns(Promise.reject(new Error("oh no")));
+  sinon.stub(AboutPages, "init");
 
   await ShieldRecipeClient.startup();
   ok(PreferenceExperiments.init.called, "startup calls PreferenceExperiments.init");
-  // Even if PreferenceExperiments.init fails, RecipeRunner.init should be called.
+  // Even if PreferenceExperiments.init fails, other init functions should be called.
+  ok(AboutPages.init.called, "startup calls AboutPages.init");
   ok(RecipeRunner.init.called, "startup calls RecipeRunner.init");
 
   PreferenceExperiments.init.restore();
   RecipeRunner.init.restore();
+  AboutPages.init.restore();
+});
+
+add_task(async function testStartupAboutPagesInitFail() {
+  sinon.stub(RecipeRunner, "init");
+  sinon.stub(PreferenceExperiments, "init");
+  sinon.stub(AboutPages, "init").returns(Promise.reject(new Error("oh no")));
+
+  await ShieldRecipeClient.startup();
+  ok(AboutPages.init.called, "startup calls AboutPages.init");
+  // Even if PreferenceExperiments.init fails, other init functions should be called.
+  ok(PreferenceExperiments.init.called, "startup calls PreferenceExperiments.init");
+  ok(RecipeRunner.init.called, "startup calls RecipeRunner.init");
+
+  PreferenceExperiments.init.restore();
+  RecipeRunner.init.restore();
+  AboutPages.init.restore();
 });
