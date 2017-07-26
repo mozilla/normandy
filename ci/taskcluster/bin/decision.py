3e443502e18734636e378fdad4d8ea8cd7e8879e
Mike Cooper <mythmon@gmail.com>
2017-07-26 15:14:21 -0700

    Cachebust addon taskcluster base image to pick up Rust 1.19.

--- ci/taskcluster/bin/decision.py
+++ ci/taskcluster/bin/decision.py
@@ -70,7 +70,7 @@ def main():
                 'deadline': fromNow('1 day'),
                 'expires': fromNow('365 days'),
                 'payload': {
-                    'image': 'mozilla/normandy-taskcluster:2017-06-12',
+                    'image': 'mozilla/normandy-taskcluster:2017-07-26',
                     'command': [
                         '/bin/bash',
                         '-c',
