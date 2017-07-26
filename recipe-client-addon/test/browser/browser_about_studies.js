4c1d6a4242c77342bdd3a79afe49b0161ef3b3f5
Michael Kelly <mkelly@mozilla.com>
2017-07-26 10:43:24 -0700

    Use ContentTask to avoid CPOWs in about:studies tests.

--- recipe-client-addon/test/browser/browser_about_studies.js
+++ recipe-client-addon/test/browser/browser_about_studies.js
@@ -1,5 +1,6 @@
 "use strict";
 
+Cu.import("resource://testing-common/ContentTask.jsm", this);
 Cu.import("resource://shield-recipe-client-content/AboutPages.jsm", this);
 
 function withAboutStudies(testFunc) {
@@ -8,10 +9,6 @@ function withAboutStudies(testFunc) {
       testFunc(...args, browser)
     ))
   );
-}
-
-function getStudyRow(doc, studyName) {
-  return doc.querySelector(`.study[data-study-name="${studyName}"]`);
 }
 
 compose_task(
@@ -27,10 +24,12 @@ compose_task(
   }),
   withAboutStudies,
   async function testLearnMore(browser) {
-    browser.contentDocument.getElementById("shield-studies-learn-more").click();
+    ContentTask.spawn(browser, null, () => {
+      content.document.getElementById("shield-studies-learn-more").click();
+    });
     await BrowserTestUtils.waitForLocationChange(gBrowser);
 
-    const location = browser.contentWindow.location.href;
+    const location = browser.currentURI.spec;
     is(
       location,
       AboutPages.aboutStudies.getShieldLearnMoreHref(),
@@ -46,12 +45,13 @@ compose_task(
   }),
   withAboutStudies,
   async function testUpdatePreferencesNewOrganization(browser) {
-    browser.contentDocument.getElementById("shield-studies-update-preferences").click();
+    ContentTask.spawn(browser, null, () => {
+      content.document.getElementById("shield-studies-update-preferences").click();
+    });
     await BrowserTestUtils.waitForLocationChange(gBrowser);
 
-    const location = browser.contentWindow.location.href;
     is(
-      location,
+      browser.currentURI.spec,
       "about:preferences#privacy-reports",
       "Clicking Update Preferences opens the privacy section of the new about:prefernces.",
     );
@@ -67,9 +67,14 @@ compose_task(
     // We have to use gBrowser instead of browser in most spots since we're
     // dealing with a new tab outside of the about:studies tab.
     const tab = await BrowserTestUtils.switchTab(gBrowser, () => {
-      browser.contentDocument.getElementById("shield-studies-update-preferences").click();
+      ContentTask.spawn(browser, null, () => {
+        content.document.getElementById("shield-studies-update-preferences").click();
+      });
     });
-    await BrowserTestUtils.waitForEvent(gBrowser.contentWindow, "load");
+
+    if (gBrowser.contentDocument.readyState !== "complete") {
+      await BrowserTestUtils.waitForEvent(gBrowser.contentWindow, "load");
+    }
 
     const location = gBrowser.contentWindow.location.href;
     is(
@@ -116,57 +121,66 @@ compose_task(
     await storage.create(study3);
 
     await BrowserTestUtils.withNewTab("about:studies", async browser => {
-      const doc = browser.contentDocument;
+      await ContentTask.spawn(browser, [study1, study2, study3], async ([cStudy1, cStudy2, cStudy3]) => {
+        Components.utils.import("resource://testing-common/ContentTaskUtils.jsm");
 
-      await BrowserTestUtils.waitForCondition(() => doc.querySelectorAll(".study-list .study").length);
-      const studyRows = doc.querySelectorAll(".study-list .study");
+        const doc = content.document;
 
-      const names = Array.from(studyRows).map(row => row.querySelector(".study-name").textContent);
-      Assert.deepEqual(
-        names,
-        [study3.name, study1.name, study2.name],
-        "Studies are sorted first by enabled status, and then by descending start date."
-      );
+        function getStudyRow(docElem, studyName) {
+          return docElem.querySelector(`.study[data-study-name="${studyName}"]`);
+        }
 
-      const study1Row = getStudyRow(doc, study1.name);
-      ok(
-        study1Row.querySelector(".study-description").textContent.includes(study1.description),
-        "Study descriptions are shown in about:studies."
-      );
-      is(
-        study1Row.querySelector(".study-status").textContent,
-        "Active",
-        "Active studies show an 'Active' indicator."
-      );
-      ok(
-        study1Row.querySelector(".remove-button"),
-        "Active studies show a remove button"
-      );
-      is(
-        study1Row.querySelector(".study-icon").textContent.toLowerCase(),
-        "a",
-        "Study icons use the first letter of the study name."
-      );
+        await ContentTaskUtils.waitForCondition(() => doc.querySelectorAll(".study-list .study").length);
+        const studyRows = doc.querySelectorAll(".study-list .study");
 
-      const study2Row = getStudyRow(doc, study2.name);
-      is(
-        study2Row.querySelector(".study-status").textContent,
-        "Complete",
-        "Inactive studies are marked as complete."
-      );
-      ok(
-        !study2Row.querySelector(".remove-button"),
-        "Inactive studies do not show a remove button"
-      );
+        const names = Array.from(studyRows).map(row => row.querySelector(".study-name").textContent);
+        Assert.deepEqual(
+          names,
+          [cStudy3.name, cStudy1.name, cStudy2.name],
+          "Studies are sorted first by enabled status, and then by descending start date."
+        );
 
-      study1Row.querySelector(".remove-button").click();
-      await BrowserTestUtils.waitForCondition(() => (
-        getStudyRow(doc, study1.name).matches(".disabled")
-      ));
-      ok(
-        getStudyRow(doc, study1.name).matches(".disabled"),
-        "Clicking the remove button updates the UI to show that the study has been disabled."
-      );
+        const study1Row = getStudyRow(doc, cStudy1.name);
+        ok(
+          study1Row.querySelector(".study-description").textContent.includes(cStudy1.description),
+          "Study descriptions are shown in about:studies."
+        );
+        is(
+          study1Row.querySelector(".study-status").textContent,
+          "Active",
+          "Active studies show an 'Active' indicator."
+        );
+        ok(
+          study1Row.querySelector(".remove-button"),
+          "Active studies show a remove button"
+        );
+        is(
+          study1Row.querySelector(".study-icon").textContent.toLowerCase(),
+          "a",
+          "Study icons use the first letter of the study name."
+        );
+
+        const study2Row = getStudyRow(doc, cStudy2.name);
+        is(
+          study2Row.querySelector(".study-status").textContent,
+          "Complete",
+          "Inactive studies are marked as complete."
+        );
+        ok(
+          !study2Row.querySelector(".remove-button"),
+          "Inactive studies do not show a remove button"
+        );
+
+        study1Row.querySelector(".remove-button").click();
+        await ContentTaskUtils.waitForCondition(() => (
+          getStudyRow(doc, cStudy1.name).matches(".disabled")
+        ));
+        ok(
+          getStudyRow(doc, cStudy1.name).matches(".disabled"),
+          "Clicking the remove button updates the UI to show that the study has been disabled."
+        );
+      });
+
       const updatedStudy1 = await storage.get(study1.name);
       ok(
         !updatedStudy1.active,
