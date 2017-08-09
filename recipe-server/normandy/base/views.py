8d428ac9a29166908368d3633ce0d12f02fa1bb9
Emin Mastizada <emin@mastizada.com>
2017-08-09 22:23:51 +0300

    fix issue #541 - favicon url

--- recipe-server/normandy/base/views.py
+++ recipe-server/normandy/base/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 
 def index(request):
     return HttpResponse('<h1>Normandy</h1>')
+
+
+def favicon(request):
+    return HttpResponseRedirect('/static/img/favicon.ico')
