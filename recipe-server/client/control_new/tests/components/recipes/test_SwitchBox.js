acad9e366026b854173bac0bf9180adc3f5d43c1
Andy Mikulski <amikulski@mozilla.com>
2017-08-16 12:05:30 -0600

    Tests

---
+++ recipe-server/client/control_new/tests/components/recipes/test_SwitchBox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import SwitchBox from 'control_new/components/forms/SwitchBox';
+
+describe('<SwitchBox>', () => {
+  const props = {
+    children: null,
+    onChange: ()=>{},
+    value: null,
+  };
+
+  it('should work', () => {
+    const wrapper = () => shallow(<SwitchBox {...props} />);
+
+    expect(wrapper).not.toThrow();
+  });
+});
