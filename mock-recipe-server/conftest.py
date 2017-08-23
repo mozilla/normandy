2b9e11217683b81b6757c6f7929a24610a9ec7c1
Michael Kelly <mkelly@mozilla.com>
2017-08-23 14:58:46 -0700

    Update APIPath usage in mock-server tests.

--- mock-recipe-server/conftest.py
+++ mock-recipe-server/conftest.py
@@ -12,4 +12,4 @@ def root_path(request):
     build_dir = request.config.getoption('--mock-server-dir')
     if not build_dir:
         pytest.fail('--mock-server-dir was not provided, but is required for this test')
-    return APIPath(build_dir, 'http://example.com/')
+    return APIPath(build_dir)
