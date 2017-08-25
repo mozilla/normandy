1f8ba0e92090eae1e774cbd491b261854fe67ca1
Michael Kelly <mkelly@mozilla.com>
2017-08-25 14:26:16 -0700

    Add extensions to admin interface in case we need to clean them up.
    Also fixes a bug with the action admin interface; a template for showing which
    recipes were using the action was missing.

--- recipe-server/normandy/recipes/admin/model_admins.py
+++ recipe-server/normandy/recipes/admin/model_admins.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.template.loader import render_to_string
 
 from normandy.recipes import models
 
@@ -16,16 +15,9 @@ class ActionAdmin(VersionAdmin):
                 'implementation_hash',
                 'implementation',
                 'arguments_schema_json',
-                'recipe_list',
-            ]
+                'recipes_used_by_html',
+            ],
         }],
     ]
 
-    readonly_fields = ['implementation_hash', 'recipe_list']
-
-    def recipe_list(self, action):
-        """List all recipes that the action is being used by."""
-        return render_to_string('admin/field_recipe_list.html', {
-            'recipes': action.recipes_used_by.order_by('name'),
-        })
-    recipe_list.short_description = 'Used in Recipes'
+    readonly_fields = ['implementation_hash', 'recipes_used_by_html']
