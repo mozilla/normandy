77c27f171d647c85b03b8e0d73d2fac3bf76121e
Andy Mikulski <amikulski@mozilla.com>
2017-09-06 14:42:15 -0600

    Bulk process recipes/extensions

--- recipe-server/client/control/state/app/revisions/reducers.js
+++ recipe-server/client/control/state/app/revisions/reducers.js
@@ -5,25 +5,40 @@ import {
   APPROVAL_REQUEST_CREATE,
   APPROVAL_REQUEST_DELETE,
   RECIPE_DELETE,
+  RECIPE_HISTORY_RECEIVE,
   REVISION_RECEIVE,
 } from 'control/state/action-types';
 
+const formatRevision = revision =>
+  revision
+    .setIn(['recipe', 'action_id'], revision.getIn(['recipe', 'action', 'id'], null))
+    .removeIn(['recipe', 'action'])
+    .set('approval_request_id', revision.getIn(['approval_request', 'id'], null))
+    .remove('approval_request')
+    .set('user_id', revision.getIn(['user', 'id'], null))
+    .remove('user');
 
 function items(state = new Map(), action) {
-  let revision;
-
   switch (action.type) {
-    case REVISION_RECEIVE:
-      revision = fromJS(action.revision);
-      revision = revision
-        .setIn(['recipe', 'action_id'], revision.getIn(['recipe', 'action', 'id'], null))
-        .removeIn(['recipe', 'action'])
-        .set('approval_request_id', revision.getIn(['approval_request', 'id'], null))
-        .remove('approval_request')
-        .set('user_id', revision.getIn(['user', 'id'], null))
-        .remove('user');
+    case RECIPE_HISTORY_RECEIVE: {
+      const revisions = fromJS(action.revisions);
+      let newState = state;
+
+      revisions.forEach(revision => {
+        const newRevision = formatRevision(revision);
+
+        newState = newState.set(revision.get('id'), newRevision);
+      });
+
+      return newState;
+    }
+
+    case REVISION_RECEIVE: {
+      let revision = fromJS(action.revision);
+      revision = formatRevision(revision);
 
       return state.set(action.revision.id, revision);
+    }
 
     case RECIPE_DELETE:
       return state.filterNot(item => item.getIn(['recipe', 'id']) === action.recipeId);
