1bc6439d4ef99e199b17f7222d2cb74501718591
Andy Mikulski <amikulski@mozilla.com>
2017-09-11 11:12:41 -0600

    Minor styling

--- recipe-server/client/control_new/components/common/ShieldIdenticon.js
+++ recipe-server/client/control_new/components/common/ShieldIdenticon.js
@@ -6,20 +6,24 @@ export default class ShieldIdenticon extends React.PureComponent {
     seed: PropTypes.string.isRequired,
     size: PropTypes.number,
     className: PropTypes.string,
-  }
+  };
 
   static defaultProps = {
     size: 64,
     className: null,
-  }
+  };
 
   render() {
     const { seed, size, className } = this.props;
 
+    if (typeof seed === 'undefined') {
+      return null;
+    }
+
     return (
       <img
         className={className}
-        src={`/api/v2/identicon/v1:${seed}.svg`}
+        src={`/api/v2/identicon/${seed}.svg`}
         height={size}
         width={size}
         alt="Shield Identicon"
