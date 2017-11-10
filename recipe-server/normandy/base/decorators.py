d12b1a150562cab6a8b241bff4c662bfd6226eb0
Rehan Dalal <rehandalal@gmail.com>
2017-11-09 20:23:33 -0500

    Remove short circuit decorator

--- recipe-server/normandy/base/decorators.py
+++ recipe-server/normandy/base/decorators.py
@@ -4,21 +4,6 @@ from django.conf import settings
 from django.views.decorators.cache import cache_control
 
 
-def short_circuit_middlewares(view_func):
-    """
-    Marks a view function as wanting to short circuit middlewares.
-    """
-    # Based on Django's csrf_exempt
-
-    # We could just do view_func.short_circuit_middlewares = True, but
-    # decorators are nicer if they don't have side-effects, so we return
-    # a new function.
-    def wrapped_view(*args, **kwargs):
-        return view_func(*args, **kwargs)
-    wrapped_view.short_circuit_middlewares = True
-    return wraps(view_func)(wrapped_view)
-
-
 def api_cache_control(**kwargs):
     """
     Adds cache headers to a view using our API cache header defaults.
--- recipe-server/normandy/selfrepair/views.py
+++ recipe-server/normandy/selfrepair/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 
-from normandy.base.decorators import api_cache_control, short_circuit_middlewares
+from normandy.base.decorators import api_cache_control
 
 
-@short_circuit_middlewares
 @api_cache_control()
 def repair(request, locale):
     return render(request, 'selfrepair/repair.html')
