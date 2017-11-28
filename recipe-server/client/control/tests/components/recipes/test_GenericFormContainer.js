09476324de58d38388a32eef6a6af5f7dcb218f0
Andy Mikulski <amikulski@mozilla.com>
2017-11-28 11:08:53 -0700

    Add tests for GenericFormContainer

---
+++ recipe-server/client/control/tests/components/recipes/test_GenericFormContainer.js
@@ -0,0 +1,102 @@
+/* eslint-disable react/prop-types, react/display-name, jsx-a11y/no-static-element-interactions */
+import React from 'react';
+import { mount } from 'enzyme';
+
+import GenericFormContainer from 'control/components/recipes/GenericFormContainer';
+
+describe('<GenericFormContainer>', () => {
+  const props = {
+    form: () => <div />,
+    formAction: async () => true,
+    onFailure: () => {},
+    onSuccess: () => {},
+    formProps: {},
+  };
+
+  it('should work', () => {
+    const wrapper = () => mount(<GenericFormContainer {...props} />);
+
+    expect(wrapper).not.toThrow();
+  });
+
+  it('should nest the `form` prop as a child', () => {
+    const CustomEl = () => <div id="test">Custom</div>;
+
+    const wrapper = mount(<GenericFormContainer {...props} form={CustomEl} />);
+
+    expect(wrapper.find('#test').length).toBe(1);
+    expect(wrapper.find('#test').text()).toContain('Custom');
+  });
+
+  it('should fire the formAction on submission', () => {
+    const CustomEl = ({ onSubmit }) => <div onClick={onSubmit}>Test</div>;
+    let fired = false;
+    const wrapper = mount(
+      <GenericFormContainer
+        {...props}
+        form={CustomEl}
+        formAction={() => {
+          fired = true;
+        }}
+      />,
+    );
+
+    wrapper.find(CustomEl).simulate('click');
+    expect(fired).toBe(true);
+  });
+
+  it('should handle the formAction failing', () => {
+    const CustomEl = ({ onSubmit }) => <div onClick={onSubmit}>Test</div>;
+    let failed = false;
+    const wrapper = mount(
+      <GenericFormContainer
+        {...props}
+        form={CustomEl}
+        onFailure={() => {
+          failed = true;
+        }}
+        formAction={() => {
+          throw new Error('Form action failed');
+        }}
+      />,
+    );
+
+    wrapper.find(CustomEl).simulate('click');
+    expect(failed).toBe(true);
+  });
+
+  it('should handle the formAction succeeding', () => {
+    const CustomEl = ({ onSubmit }) => <div onClick={onSubmit}>Test</div>;
+    let success = false;
+    const wrapper = mount(
+      <GenericFormContainer
+        {...props}
+        form={CustomEl}
+        onSuccess={() => {
+          success = true;
+        }}
+      />,
+    );
+
+    wrapper.find(CustomEl).simulate('click');
+    // formAction is async so we need to wait for that promise to resolve.
+    setTimeout(() => {
+      expect(success).toBe(true);
+    }, 1);
+  });
+
+  it('should pass the formProps object to the form element', () => {
+    const CustomEl = custProps => <div id="test" {...custProps}>Custom</div>;
+    const testProps = { test: 1, woo: 'hoo' };
+    const wrapper = mount(
+      <GenericFormContainer
+        {...props}
+        form={CustomEl}
+        formProps={testProps}
+      />,
+    );
+
+    expect(wrapper.find('#test').props().test).toBe(1);
+    expect(wrapper.find('#test').props().woo).toBe('hoo');
+  });
+});
