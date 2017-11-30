ade85f3e9cc85cbd6cb8c3a668d27c6fc34b45db
Mike Cooper <mythmon@gmail.com>
2017-11-29 16:02:02 -0800

    Add tests for router selectors.

--- recipe-server/client/control/tests/state/index.js
+++ recipe-server/client/control/tests/state/index.js
@@ -5,6 +5,7 @@ import { INITIAL_STATE as extensions } from 'control/tests/state/extensions';
 import { INITIAL_STATE as recipes } from 'control/tests/state/recipes';
 import { INITIAL_STATE as requests } from 'control/tests/state/requests';
 import { INITIAL_STATE as revisions } from 'control/tests/state/revisions';
+import { INITIAL_STATE as router } from 'control/tests/state/router';
 import { INITIAL_STATE as serviceInfo } from 'control/tests/state/serviceInfo';
 import { INITIAL_STATE as users } from 'control/tests/state/users';
 
@@ -20,4 +21,5 @@ export const INITIAL_STATE = {
     serviceInfo,
     users,
   },
+  router,
 };
---
+++ recipe-server/client/control/tests/state/router/test_selectors.js
@@ -0,0 +1,117 @@
+import { applyMiddleware, compose, createStore } from 'redux';
+import { push, initializeCurrentLocation } from 'redux-little-router';
+
+import reducers from 'control/state';
+import { enhancer as routerEnhancer, middleware as routerMiddleware } from 'control/routes';
+import {
+  getUrlParam,
+  getUrlParamAsInt,
+  getQueryParam,
+  getQueryParamAsInt,
+  getCurrentURL,
+  getRouterPath,
+  getBreadcrumbs,
+} from 'control/state/router/selectors';
+
+
+describe('route selectors', () => {
+  let store;
+  const middleware = [
+    routerMiddleware,
+  ];
+
+  beforeEach(() => {
+    store = createStore(reducers, undefined, compose(
+      applyMiddleware(...middleware),
+      routerEnhancer,
+    ));
+
+    const initialLocation = store.getState().router;
+    if (initialLocation) {
+      store.dispatch(initializeCurrentLocation(initialLocation));
+    }
+  });
+
+  describe('getUrlParam', () => {
+    it('works', () => {
+      // Using route /recipe/:recipeId/
+      store.dispatch(push('/recipe/42/'));
+      const val = getUrlParam(store.getState(), 'recipeId');
+      expect(val).toEqual('42');
+    });
+
+    it('returns the default', () => {
+      // Using route /recipe/:recipeId/
+      store.dispatch(push('/recipe/42/'));
+      const val = getUrlParam(store.getState(), 'nonexistent', 'default');
+      expect(val).toEqual('default');
+    });
+  });
+
+  describe('getUrlParamAsInt', () => {
+    it('works', () => {
+      // Using route /recipe/:recipeId/
+      store.dispatch(push('/recipe/42/'));
+      const val = getUrlParamAsInt(store.getState(), 'recipeId');
+      expect(val).toEqual(42);
+    });
+
+    it('returns the default', () => {
+      // Using route /recipe/:recipeId/
+      store.dispatch(push('/recipe/42/'));
+      const val = getUrlParamAsInt(store.getState(), 'nonexistent', 64);
+      expect(val).toEqual(64);
+    });
+  });
+
+  describe('getQueryParam', () => {
+    it('works', () => {
+      store.dispatch(push('/?foo=bar'));
+      const val = getQueryParam(store.getState(), 'foo');
+      expect(val).toEqual('bar');
+    });
+
+    it('returns the default', () => {
+      store.dispatch(push('/?foo=bar'));
+      const val = getQueryParam(store.getState(), 'nonexistent', 'default');
+      expect(val).toEqual('default');
+    });
+  });
+
+  describe('getQueryParamAsInt', () => {
+    it('works', () => {
+      store.dispatch(push('/?x=42'));
+      const val = getQueryParamAsInt(store.getState(), 'x');
+      expect(val).toEqual(42);
+    });
+
+    it('returns the default', () => {
+      store.dispatch(push('/?x=42'));
+      const val = getQueryParamAsInt(store.getState(), 'nonexistent', 64);
+      expect(val).toEqual(64);
+    });
+  });
+
+  describe('getCurrentURL', () => {
+    it('works', () => {
+      // using route /recipe/:recipeId/
+      store.dispatch(push('/recipe/42/?old=foo'));
+      const val = getCurrentURL(store.getState(), {new: 'bar'});
+      expect(val).toEqual({
+        pathname: '/recipe/42/',
+        query: {
+          old: 'foo',
+          new: 'bar',
+        },
+      });
+    });
+  });
+
+  describe('getRouterPath', () => {
+    it('works', () => {
+      // using route /recipe/:recipeId/
+      store.dispatch(push('/recipe/42/?old=foo'));
+      const val = getRouterPath(store.getState());
+      expect(val).toEqual('/recipe/42/');
+    });
+  });
