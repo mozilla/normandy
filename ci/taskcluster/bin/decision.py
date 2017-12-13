cefb45787334e6d972066cfa35a21ba6aafa5e30
Mike Cooper <mythmon@gmail.com>
2017-12-12 18:49:43 -0600

    Cachebust add-on builder Dockerfile to pick up new Rust version.

--- ci/taskcluster/bin/decision.py
+++ ci/taskcluster/bin/decision.py
@@ -70,7 +70,7 @@ def main():
                 'deadline': fromNow('1 day'),
                 'expires': fromNow('365 days'),
                 'payload': {
-                    'image': 'mozilla/normandy-taskcluster:2017-10-16',
+                    'image': 'mozilla/normandy-taskcluster:2017-12-12',
                     'command': [
                         '/bin/bash',
                         '-c',
