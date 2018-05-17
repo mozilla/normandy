2122ed200b447ff8df6dc4dc38c4234ad4c28a3e
Mike Cooper <mythmon@gmail.com>
2018-05-16 17:04:36 -0700

    Send statsd signals on heartbeat page loads
    This was a regression from #1337. This is the method recommended by
    python-dockerflow.

--- normandy/health/apps.py
+++ normandy/health/apps.py
@@ -5,3 +5,7 @@ class HealthApp(AppConfig):
     name = 'normandy.health'
     label = 'health'
     verbose_name = 'Normandy Health'
+
+    def ready(self):
+        # Import for side-effect: registers signal handler
+        import normandy.health.signals  # NOQA
---
+++ normandy/health/signals.py
@@ -0,0 +1,14 @@
+from django.dispatch import receiver
+
+from dockerflow.django.signals import heartbeat_passed, heartbeat_failed
+from statsd.defaults.django import statsd
+
+
+@receiver(heartbeat_passed)
+def heartbeat_passed_handler(sender, level, **kwargs):
+    statsd.incr('heartbeat.pass')
+
+
+@receiver(heartbeat_failed)
+def heartbeat_failed_handler(sender, level, **kwargs):
+    statsd.incr('heartbeat.fail')
