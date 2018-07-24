8089c676d94d13697260b06326f6188bd21853ff
Mike Cooper <mythmon@gmail.com>
2018-07-24 15:57:22 -0700

    Stop serving the control interface

--- normandy/base/tests/test_views.py
+++ normandy/base/tests/test_views.py
@@ -1,13 +1,3 @@
-from django.test import override_settings
-
-
 def test_index(client):
-    response = client.get("/")
-    assert response.status_code == 302
-    assert response.url == "/login/?next=/"
-
-
-@override_settings(ADMIN_ENABLED=False)
-def test_index_admin_disabled(client):
     response = client.get("/")
     assert response.status_code == 200
