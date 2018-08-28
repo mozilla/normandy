f66407ac745c130dc35554a70dc425ad1022c4be
Mike Cooper <mythmon@gmail.com>
2018-08-28 16:34:05 -0700

    Upgrade babel and related packages to latest versions

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-function-bind',
+    '@babel/plugin-transform-modules-commonjs',
+    'babel-plugin-syntax-async-functions',
+  ],
+};
