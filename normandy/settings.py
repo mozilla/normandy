90304c1a26e2bba5aa32e4afc82cdf8af7e9f40a
tiftran <ttran@mozilla.com>
2019-08-02 10:45:43 -0700

    formatting and linting

--- normandy/settings.py
+++ normandy/settings.py
@@ -8,7 +8,6 @@ from corsheaders.defaults import default_methods
 class Core(Configuration):
     """Settings that will never change per-environment."""
 
-
     # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
