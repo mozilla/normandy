54535fd6651546295f4edd034e65cc6118b49c5a
Chris Hartjes <chartjes@littlehart.net>
2019-10-10 11:49:09 -0400

    Updated code to handle scenarios where no data exists more gracefully

--- contract-tests/v3_api/test_recipe_history.py
+++ contract-tests/v3_api/test_recipe_history.py
@@ -1,11 +1,17 @@
+import pytest
+
 from random import randint
 from support.assertions import assert_valid_schema
 
 
-def test_approval_request_read(conf, requests_session):
+def test_recipe_history(conf, requests_session):
     # Get the ID of a random recipe and grab it's history
     response = requests_session.get(conf.getoption("server") + "/api/v3/recipe/")
     data = response.json()
+
+    if len(data["results"]) == 0:
+        pytest.skip("Could not find any recipe history")
+
     idx = randint(0, len(data["results"]) - 1)
     id = data["results"][idx]["id"]
     response = requests_session.get(
--- contract-tests/v3_api/test_recipe_revision_read.py
+++ contract-tests/v3_api/test_recipe_revision_read.py
@@ -1,3 +1,5 @@
+import pytest
+
 from random import randint
 from support.assertions import assert_valid_schema
 
@@ -6,6 +8,10 @@ def test_recipe_revision_read(conf, requests_session):
     # Get a random recipe revision from the list and verify it
     response = requests_session.get(conf.getoption("server") + "/api/v3/recipe_revision/")
     data = response.json()
+
+    if len(data["results"]) == 0:
+        pytest.skip("Could not find any recipe revisions")
+
     idx = randint(0, len(data["results"]) - 1)
     id = data["results"][idx]["id"]
     response = requests_session.get(
