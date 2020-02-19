74ad73a7de804f3655104ad3c57b26baf7769911
Alissa Sobo <alissasobo@gmail.com>
2020-02-19 13:05:56 -0800

    fix docstring

--- normandy/recipes/filters.py
+++ normandy/recipes/filters.py
@@ -396,21 +396,21 @@ class DateRangeFilter(BaseFilter):
 
 class ProfileCreateDateFilter(BaseFilter):
     """
-        This filter is meant to distinguish between new and existing users.
-        Target users who have a profile creation date older than or newer than
-        a given date. This date must be specified in days since January 1st 1970.
+    This filter is meant to distinguish between new and existing users.
+    Target users who have a profile creation date older than or newer than
+    a given date.
 
-        .. attribute:: type
+    .. attribute:: type
 
-            ``profile_creation_date``
+        ``profile_creation_date``
 
-        .. attribute:: direction
+    .. attribute:: direction
 
-           :example: ``newer_than``
+       :example: ``newer_than``
 
-       .. attribute:: date
+   .. attribute:: date
 
-          :example: ``18657``
+      :example: ``2020-02-01``
     """
 
     type = "profile_creation_date"
