98c20aa2a8469d48c0ebc34aabd4ce8423194a37
Chris Hartjes <chartjes@grumpy-learning.com>
2020-04-02 16:06:19 -0400

    Removed unused variable as per flake8

--- contract-tests/v3_api/conftest.py
+++ contract-tests/v3_api/conftest.py
@@ -4,4 +4,4 @@ import pytest
 @pytest.fixture(scope="session")
 def headers():
     # Return the authorization header that we need
-    return {"Authorization": "Insecure test-user@example.com".format(email)}
+    return {"Authorization": "Insecure test-user@example.com"}
