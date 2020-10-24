baa3953ef46878c2a9fa7a4a17afd6ce3644cded
tiftran <ttran@mozilla.com>
2020-10-23 17:28:16 -0700

    edited imports to drf_yasg2

--- normandy/base/api/swagger_urls.py
+++ normandy/base/api/swagger_urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+from drf_yasg2.views import get_schema_view
+from drf_yasg2 import openapi
 from rest_framework import permissions
 
 
